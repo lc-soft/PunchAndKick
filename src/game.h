4dde1315a11b485484f852f50d2df532cdfc456e
Liu Chao <lc-soft@live.cn>
2013-07-04 21:42:54 +0800

    更新头文件内的函数声明

--- src/game.h
+++ src/game.h
@@ -1,4 +1,18 @@
 
-extern int Game_LoadMenuRes(void);
+extern int Game_LoadMainMenuRes(void);
 
-extern void Game_MainMenu(void);
+extern void DrawSelectBox( LCUI_Graph *buff );
+
+extern void Game_InitMainMenu(void);
+extern void Game_ShowMainMenu(void);
+extern void Game_HideMainMenu(void);
+
+/* 初始化局域网对战 */
+extern void Game_InitLANBattle(void);
+
+
+extern int Game_Init(void);
+
+extern int Game_Start(void);
+
+extern int Game_Pause(void);
