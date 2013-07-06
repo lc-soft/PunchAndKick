1739c6e2a88e7e2d771b09589e4f32eb564d6de3
Liu Chao <lc-soft@live.cn>
2013-07-06 17:59:57 +0800

    更新头文件

--- src/game.h
+++ src/game.h
@@ -1,18 +1,5 @@
 
-extern int Game_LoadMainMenuRes(void);
-
-extern void DrawSelectBox( LCUI_Graph *buff );
-
-extern void Game_InitMainMenu(void);
-extern void Game_ShowMainMenu(void);
-extern void Game_HideMainMenu(void);
-
-/* 初始化局域网对战 */
-extern void Game_InitLANBattle(void);
-
-
-extern int Game_Init(void);
-
-extern int Game_Start(void);
-
-extern int Game_Pause(void);
+#include "game_object.h"
+#include "game_control.h"
+#include "game_msg.h"
+#include "main_menu.h"
---
+++ src/main_menu.h
@@ -0,0 +1,6 @@
+﻿
+extern void Game_InitMainMenu(void);
+extern void Game_ShowMainMenu(void);
+extern void Game_HideMainMenu(void);
+
+extern int Game_LoadMainMenuRes(void);
