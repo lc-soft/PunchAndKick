e1ec7dd1693e4e314f6526fadaad556bc762a0ea
Liu Chao <lc-soft@live.cn>
2013-07-06 17:57:41 +0800

    更新消息模块的代码

---
+++ src/game_msg.c
@@ -0,0 +1,153 @@
+﻿#include <LCUI_Build.h>
+#include LC_LCUI_H
+#include LC_WIDGET_H
+#include LC_INPUT_H
+#include "game.h"
+#include "game_object.h"
+
+typedef struct GamePlayerComputer_ {
+	char name[256];
+	//....
+} GamePlayerComputer;
+
+static LCUI_Thread msg_thread_id = 0;
+static LCUI_Queue game_msg_queue;		/**< 游戏的消息队列 */
+static LCUI_BOOL enable_lan_battle = FALSE;	/**< 是否启用局域网对战 */
+static GamePlayerComputer second_computer;	/**< 对方玩家计算机信息 */
+
+/* 向对方主机发送消息 */
+static int Game_SendMsg( GameMsg *msg )
+{
+	/* 若启用局域网对战 */
+	if( enable_lan_battle ) {
+		//...
+	}
+	return 0;
+}
+
+/* 接收对方主机发送过来的消息 */
+static int Game_RecvMsg( void )
+{
+	if( enable_lan_battle ) {
+		//...
+	}
+	return 0;
+}
+
+/* 向消息队列中投递消息 */
+int Game_PostMsg( GameMsg *msg )
+{
+	int ret;
+	Queue_Lock( &game_msg_queue );
+	ret = Queue_Add( &game_msg_queue, msg );
+	Queue_Unlock( &game_msg_queue );
+	return ret>=0?0:-1;
+}
+
+/* 从消息队列中取出一个消息 */
+int Game_GetMsg( GameMsg *msg_buff )
+{
+	GameMsg *p_msg;
+	Queue_Lock( &game_msg_queue );
+	p_msg = (GameMsg*)Queue_Get( &game_msg_queue, 0 );
+	if( p_msg == NULL ) {
+		Queue_Unlock( &game_msg_queue );
+		return -1;
+	}
+	*msg_buff = *p_msg;
+	Queue_Delete( &game_msg_queue, 0 );
+	Queue_Unlock( &game_msg_queue );
+	return 0;
+}
+
+/* 处理消息 */
+int Game_ProcMsg( GameMsg *p_msg )
+{
+	GamePlayer *target;
+
+	target = GamePlayer_GetByID( p_msg->player_id );
+	if( target == NULL ) {
+		return -1;
+	}
+	switch( p_msg->msg.msg_id ) {
+	case GAMEMSG_STATE:
+		GameObject_SwitchAction( target->object, p_msg->msg.state.state );
+	default:
+		break;
+	}
+	return 0;
+}
+
+static void GameMsgProcThread( void *arg )
+{
+	int ret;
+	GameMsg msg;
+
+	while(1) {
+		Game_RecvMsg();
+		ret = Game_GetMsg( &msg );
+		if( ret == 0 ) {
+			Game_ProcMsg( &msg );
+		} else {
+			LCUI_MSleep( 10 );
+		}
+	}
+}
+
+int GameMsgLoopStart( void )
+{
+	if( msg_thread_id != 0 ) {
+		return -1;
+	}
+	Queue_Init( &game_msg_queue, sizeof(GameMsg), NULL );
+	return LCUIThread_Create( &msg_thread_id, GameMsgProcThread, NULL );
+}
+
+void GameMsgLoopStop( void )
+{
+
+}
+
+static int pressed_key_code = 0;
+
+static void GameKeyboardProcKeyDown( int key_code )
+{
+	GamePlayer *target;
+	
+	target = GamePlayer_GetPlayerByControlKey( key_code );
+	switch(key_code) {
+	case LCUIKEY_D:
+		target->right_direction = TRUE;
+		GamePlayer_ChangeState( target, STATE_WALK );
+		break;
+	case LCUIKEY_A:
+		target->right_direction = FALSE;
+		GamePlayer_ChangeState( target, STATE_WALK );
+		break;
+	default:
+		break;
+	}
+}
+
+static void GameKeyboardProcKeyUp( int key_code )
+{
+	GamePlayer *target;
+	
+	target = GamePlayer_GetPlayerByControlKey( key_code );
+	switch(key_code) {
+	case LCUIKEY_D:
+	case LCUIKEY_A:
+	default:
+		break;
+	}
+	GamePlayer_ChangeState( target, STATE_STANCE );
+}
+
+void GameKeyboardProc( LCUI_KeyboardEvent *event, void *arg )
+{
+	if( event->type == LCUI_KEYDOWN ) {
+		GameKeyboardProcKeyDown( event->key_code );
+	} else {
+		GameKeyboardProcKeyUp( event->key_code );
+	}
+}
---
+++ src/game_msg.h
@@ -0,0 +1,41 @@
+﻿
+enum GameMsgID {
+	GAMEMSG_MOTION,
+	GAMEMSG_STATE,
+	GAMEMSG_HP
+};
+
+typedef struct GameMotionMsg_ {
+	int msg_id;
+	int x, y;	/**< 角色坐标 */
+} GameMotionMsg;
+
+typedef struct GameStateMsg_ {
+	int msg_id;
+	int state;	/**< 角色状态 */
+} GameStateMsg;
+
+typedef struct GameHPMsg_ {
+	int msg_id;
+	int hp;		/**< 角色生命值 */
+} GameHPMsg;
+
+typedef union GameMsgBuff_ {
+	int msg_id;		/**< 消息ID */
+	GameMotionMsg motion;	/**< 角色移动消息 */
+	GameStateMsg state;	/**< 角色状态消息 */
+	GameHPMsg hp;		/**< 角色生命值消息 */
+} GameMsgBuff;
+
+
+typedef struct GameMsg {
+	int player_id;		/**< 针对的角色的ID */
+	GameMsgBuff msg;	/**< 消息内容 */
+} GameMsg;
+
+extern void GameKeyboardProc( LCUI_KeyboardEvent *event, void *arg );
+
+/** 向消息队列中投递消息 */
+extern int Game_PostMsg( GameMsg *msg );
+
+extern int GameMsgLoopStart( void );
