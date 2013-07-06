e3b99f9dc9f9ad9f408f8da3151ad7b4f72f5692
Liu Chao <lc-soft@live.cn>
2013-07-06 17:58:02 +0800

    更新角色控制模块的代码

---
+++ src/game_control.c
@@ -0,0 +1,191 @@
+#include <LCUI_Build.h>
+#include LC_LCUI_H
+#include LC_WIDGET_H
+#include LC_ACTIVEBOX_H
+
+#include "game.h"
+
+GamePlayer player_data[4];
+
+/** 载入角色的移动动作动画资源 */
+static ActionData* ActionRes_LoadWalk(void)
+{
+	int i;
+	ActionData *action;
+	LCUI_Graph img_move[8];
+	char path[512];
+
+	action = Action_Create();
+	for(i=0; i<8; ++i) {
+		Graph_Init( &img_move[i] );
+		sprintf( path, "drawable/walk-%02d.png", i+1 );
+		Graph_LoadImage( path, &img_move[i] );
+		Action_AddFrame( action, -6,0, &img_move[i], 100 );
+	}
+	return action;
+}
+
+/** 载入角色的站立动作动画资源 */
+static ActionData* ActionRes_LoadStance(void)
+{
+	int i;
+	ActionData *action;
+	LCUI_Graph img_stance[4];
+	char path[512];
+	
+	action = Action_Create();
+	for(i=0; i<4; ++i) {
+		Graph_Init( &img_stance[i] );
+		sprintf( path, "drawable/stance-%02d.png", i+1 );
+		Graph_LoadImage( path, &img_stance[i] );
+		Action_AddFrame( action, -5,0, &img_stance[i], 100 );
+	}
+	return action;
+}
+
+/** 载入角色的动作动画资源 */
+ActionData* LoadGamePlayerRes( int action_type )
+{
+	switch( action_type ) {
+	case STATE_STANCE: return ActionRes_LoadStance();
+	case STATE_WALK: return ActionRes_LoadWalk();
+	}
+	return NULL;
+}
+
+int GamePlayer_InitAction( GamePlayer *player )
+{
+	ActionData* action;
+
+	player->state = STATE_STANCE;
+	/* 创建GameObject部件 */
+	player->object = GameObject_New();
+	/* 载入游戏角色资源 */
+	action = LoadGamePlayerRes( STATE_STANCE );
+	/* 将动作集添加至游戏对象 */
+	GameObject_AddAction( player->object, action, STATE_STANCE );
+	
+	action = LoadGamePlayerRes( STATE_WALK );
+	GameObject_AddAction( player->object, action, STATE_WALK );
+
+	/* 移动游戏角色的位置 */
+	GameObject_Move( player->object, 300, 300 );
+	//Widget_SetBorder( player->object, Border(1,BORDER_STYLE_SOLID, RGB(0,0,0)) );
+	Widget_Show( player->object );
+	return 0;
+}
+
+int Game_Init(void)
+{
+	int ret;
+	/* 注册GameObject部件 */
+	GameObject_Register();
+	/* 记录玩家ID */
+	player_data[0].id = 1;
+	player_data[1].id = 2;
+	player_data[2].id = 3;
+	player_data[3].id = 4;
+	/* 设置基本属性 */
+	player_data[0].walk_speed = 100;
+	player_data[0].run_speed = 100;
+	player_data[0].human_control = TRUE;
+	player_data[0].local_control = TRUE;
+	player_data[0].right_direction = TRUE;
+	/* 初始化角色动作动画 */
+	ret = GamePlayer_InitAction( &player_data[0] );
+	//Game_InitPlayerAction( &player_data[1] );
+	//Game_InitPlayerAction( &player_data[2] );
+	//Game_InitPlayerAction( &player_data[3] );
+	/* 响应按键输入 */
+	ret |= LCUI_KeyboardEvent_Connect( GameKeyboardProc, NULL );
+	ret |= GameMsgLoopStart();
+	return ret;
+}
+
+static void GamePlayer_ProcState( GamePlayer *player, int state )
+{
+	double n;
+	int x, y;
+
+	switch( state ) {
+	case STATE_WALK:
+		n = 1.0 * player->walk_speed;
+		n = n*2 / 100;
+		GameObject_GetPos( player->object, &x, &y );
+		if( player->right_direction ) {
+			x += (int)n;
+		} else {
+			x -= (int)n;
+		}
+		GameObject_Move( player->object, x, y );
+	default:
+		break;
+	}
+}
+
+static void GamePlayer_Control( void *arg )
+{
+	int i;
+	while(1) {
+		/* 总玩家数为4个，由于还处于开发阶段，先设为1 */
+		for(i=0; i<1; ++i) {
+			if( !player_data[i].local_control ) {
+				continue;
+			}
+			GamePlayer_ProcState( &player_data[i], player_data[i].state );
+		}
+		LCUI_MSleep( 10 );
+	}
+}
+
+int Game_Start(void)
+{
+	LCUI_Thread t;
+	LCUIThread_Create( &t, GamePlayer_Control, NULL );
+	GameObject_PlayAction( player_data[0].object );
+	//GameObject_PlayAction( player_data[1].object );
+	//GameObject_PlayAction( player_data[2].object );
+	//GameObject_PlayAction( player_data[3].object );
+	return 0;
+}
+
+int Game_Pause(void)
+{
+	return 0;
+}
+
+/** 通过控制键获取该键控制的角色 */
+GamePlayer *GamePlayer_GetPlayerByControlKey( int key_code )
+{
+	return &player_data[0];
+}
+
+/** 通过角色ID来获取角色 */
+GamePlayer *GamePlayer_GetByID( int player_id )
+{
+	if( player_id > 4 ) {
+		return NULL;
+	}
+	return &player_data[player_id-1];
+}
+
+/** 改变角色的状态  */
+void GamePlayer_ChangeState( GamePlayer *player, int state )
+{
+	GameMsg msg;
+
+	switch(state) {
+	case STATE_RUN:
+	case STATE_STANCE:
+	default:
+		break;
+	}
+	/* 若状态不一致 */
+	if( player->state != state ) {
+		msg.player_id = player->id;
+		msg.msg.msg_id = GAMEMSG_STATE;
+		msg.msg.state.state = state;
+		Game_PostMsg( &msg );
+		player->state = state;
+	}
+}
---
+++ src/game_control.h
@@ -0,0 +1,32 @@
+﻿
+enum GamePlayerState {
+	STATE_STANCE,	/**< 站立 */
+	STATE_WALK,	/**< 步行 */
+	STATE_RUN,	/**< 奔跑 */
+	STATE_HIT,	/**< 被命中 */
+	STATE_REST	/**< 歇息 */
+};
+
+typedef struct GamePlayer_ {
+	int id;				/**< 玩家ID */
+	LCUI_BOOL right_direction;	/**< 角色是否朝着右方 */
+	LCUI_BOOL human_control;	/**< 是否由人类控制 */
+	LCUI_BOOL local_control;	/**< 是否由此处玩家控制 */
+	int state;			/**< 玩家当前状态 */
+	int hp, mp;			/**< 生命值和法力值 */
+	int walk_speed;			/**< 步行的速度 */
+	int run_speed;			/**< 奔跑的速度 */
+	LCUI_Widget *object;		/**< 游戏对象 */
+} GamePlayer;
+
+extern int Game_Init(void);
+extern int Game_Start(void);
+extern int Game_Pause(void);
+
+extern void GamePlayer_ChangeState( GamePlayer *player, int state );
+
+/** 通过控制键获取该键控制的角色 */
+extern GamePlayer *GamePlayer_GetPlayerByControlKey( int key_code );
+
+/** 通过角色ID来获取角色 */
+extern GamePlayer *GamePlayer_GetByID( int player_id );
