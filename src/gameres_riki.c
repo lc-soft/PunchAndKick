fd0bb7bf07103c1f9dc9d9aa2b4cfc8f628f5825
Liu Chao <lc-soft@live.cn>
2013-07-11 21:24:14 +0800

    添加READY动作动画，调整A攻击的动作速度

--- src/gameres_riki.c
+++ src/gameres_riki.c
@@ -44,6 +44,23 @@ static ActionData* ActionRes_LoadStance(void)
 	return action;
 }
 
+/** 载入角色的站立动作动画资源 */
+static ActionData* ActionRes_LoadReady(void)
+{
+	int i;
+	ActionData *action;
+	LCUI_Graph img_ready[1];
+	char path[512];
+	
+	action = Action_Create();
+	for(i=0; i<1; ++i) {
+		Graph_Init( &img_ready[i] );
+		sprintf( path, "drawable/ready-%02d.png", i+1 );
+		Graph_LoadImage( path, &img_ready[i] );
+		Action_AddFrame( action, -5,0, &img_ready[i], 50 );
+	}
+	return action;
+}
 /** 载入角色的奔跑动作动画资源 */
 static ActionData* ActionRes_LoadRun(void)
 {
@@ -72,17 +89,17 @@ static ActionData* ActionRes_LoadAAttack(void)
 
 	Graph_Init( &img_attack[0] );
 	Graph_LoadImage( "drawable/A-attack-01.png", &img_attack[0] );
-	Action_AddFrame( action, -5,0, &img_attack[0], 5 );
+	Action_AddFrame( action, -5,0, &img_attack[0], 3 );
 	
 	Graph_Init( &img_attack[0] );
 	Graph_LoadImage( "drawable/A-attack-02.png", &img_attack[1] );
-	Action_AddFrame( action, -5,0, &img_attack[1], 5 );
+	Action_AddFrame( action, -5,0, &img_attack[1], 3 );
 
 	Graph_Init( &img_attack[0] );
 	Graph_LoadImage( "drawable/A-attack-03.png", &img_attack[2] );
-	Action_AddFrame( action, -5,0, &img_attack[2], 5 );
+	Action_AddFrame( action, -5,0, &img_attack[2], 3 );
 
-	Action_AddFrame( action, -5,0, &img_attack[1], 5 );
+	Action_AddFrame( action, -5,0, &img_attack[1], 3 );
 
 	return action;
 }
@@ -113,6 +130,7 @@ ActionData* ActionRes_LoadRiki( int action_type )
 	case ACTION_RUN: return ActionRes_LoadRun();
 	case ACTION_A_ATTACK: return ActionRes_LoadAAttack();
 	case ACTION_B_ATTACK: return ActionRes_LoadBAttack();
+	case ACTION_READY: return ActionRes_LoadReady();
 	default:break;
 	}
 	return NULL;
