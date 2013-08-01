ae0e5afdb28fc0470c6e6420452eb42f624bb0a3
Liu Chao <lc-soft@live.cn>
2013-08-01 17:06:44 +0800

    修改游戏启动画面

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-﻿#define SKIP_BOOT_SCREEN
+﻿//#define SKIP_BOOT_SCREEN
 #define I_NEED_WINMAIN
 #include <LCUI_Build.h>
 #include LC_LCUI_H
@@ -9,31 +9,19 @@
 #include LC_LABEL_H
 #include "game.h"
 
-#define IMG_PATH_GAME_LOGO	"drawable/lcui-logo.png"
-#define IMG_PATH_RING		"drawable/ring.png"
-#define IMG_PATH_LC_LOGO	"drawable/logo.png"
-#define TEXT_COPYRIGHT		L"Powered by <color=#ff0000>LCUI</color>"
-
-static LCUI_Graph img_game_logo, img_bg, img_ring, img_lc_logo;
-
-static void GraphRes_Load(void)
-{
-	Graph_Init( &img_game_logo );
-	Graph_Init( &img_lc_logo );
-	Graph_Init( &img_ring );
-	Graph_LoadImage( IMG_PATH_GAME_LOGO, &img_game_logo );
-	Graph_LoadImage( IMG_PATH_LC_LOGO, &img_lc_logo );
-	Graph_LoadImage( IMG_PATH_RING, &img_ring );
-}
+#define TEXT_COPYRIGHT		L"Developed by LC-Games."
 
 static void Game_ShowBootScreen(void)
 {
 	uchar_t alpha;
 	LCUI_Widget *wdg_img_logo, *wdg_text;
+	LCUI_Graph img_game_logo;
 
 	wdg_img_logo = Widget_New(NULL);
 	wdg_text = Widget_New("label");
 	
+	Graph_Init( &img_game_logo );
+	GameGraphRes_GetGraph(MAIN_RES, "main-logo", &img_game_logo );
 	Label_TextW( wdg_text, TEXT_COPYRIGHT );
 	Widget_Resize( wdg_img_logo, Graph_GetSize(&img_game_logo) );
 	Widget_SetAlign( wdg_text, ALIGN_BOTTOM_CENTER, Pos(0,-20) );
@@ -68,151 +56,51 @@ static void Game_ShowBootScreen(void)
 	Widget_Destroy( wdg_text );
 }
 
-static LCUI_BOOL res_load_state = 0;
-
-int Game_LoadMenuRes( void )
+static void UpdateViewFPS( void *arg )
 {
-	int ret;
-	ret = Game_LoadMainMenuRes();
-	return ret;
+	char str[10];
+	LCUI_Widget *label = (LCUI_Widget*)arg;
+	sprintf( str, "FPS: %d", LCUIScreen_GetFPS() );
+	Label_Text( label, str );
 }
 
-/* 载入游戏数据 */
-static void Game_LoadData( void *arg )
+static void InitViewFPS( void )
 {
-	LCUI_Widget *wdg_label;
-
-	wdg_label = (LCUI_Widget*)arg;
-	Label_TextW( wdg_label, L"载入主菜单资源..." );
-	LCUI_MSleep(100);
-	if( Game_LoadMenuRes() != 0 ) {
-		Label_TextW( wdg_label, L"主菜单资源载入失败！" );
-		res_load_state = -1;
-		LCUIThread_Exit(NULL);
-	}
-	Label_TextW( wdg_label, L"载入角色数据..." );
-	LCUI_MSleep(500);
-	Label_TextW( wdg_label, L"已载入完成" );
-	res_load_state = 1;
-	LCUIThread_Exit(NULL);
-}
-
-static int Game_ShowLoadingScreen(void)
-{
-	int angle = 0;
-	LCUI_Thread t;
-	uchar_t alpha;
+	LCUI_Widget *widget;
 	LCUI_TextStyle style;
-	LCUI_Graph img_buff;
-	LCUI_Widget *wdg_logo, *wdg_ring, *wdg_text;
-	
-	Graph_Init( &img_buff );
+
+	widget = Widget_New("label");
 	TextStyle_Init( &style );
-	TextStyle_FontSize( &style, 18 );
-
-	wdg_logo = Widget_New(NULL);
-	wdg_ring = Widget_New(NULL);
-	wdg_text = Widget_New("label");
-	Widget_Container_Add( wdg_logo, wdg_ring );
-	Widget_Resize( wdg_logo, Size(128,128) );
-	Widget_Resize( wdg_ring, Size(128,128) );
-	Widget_SetBackgroundImage( wdg_ring, &img_ring );
-	Widget_SetBackgroundImage( wdg_logo, &img_lc_logo );
-	Widget_SetBackgroundLayout( wdg_logo, LAYOUT_CENTER );
-	Widget_SetBackgroundLayout( wdg_ring, LAYOUT_CENTER );
-	Widget_SetAlign( wdg_logo, ALIGN_MIDDLE_CENTER, Pos(0,-20) );
-	Widget_SetAlign( wdg_ring, ALIGN_MIDDLE_CENTER, Pos(0,0) );
-	Widget_SetAlign( wdg_text, ALIGN_MIDDLE_CENTER, Pos(0,80) );
-	Widget_SetAlpha( wdg_logo, 0 );
-	Widget_SetAlpha( wdg_ring, 0 );
-	Widget_SetAlpha( wdg_text, 0 );
-	Widget_Show( wdg_logo );
-	Widget_Show( wdg_ring );
-	Widget_Show( wdg_text );
-
-	Label_TextStyle( wdg_text, style );
-	Label_TextW( wdg_text, L"正初始化..." );
-#ifndef SKIP_BOOT_SCREEN
-	/* 以淡入的效果显示LOGO */
-	for( alpha=0; alpha<250; alpha+=10 ) {
-		Widget_SetAlpha( wdg_logo, alpha );
-		Widget_SetAlpha( wdg_text, alpha );
-		LCUI_MSleep(25);
-	}
-#endif
-	Widget_SetAlpha( wdg_logo, 255 );
-	Widget_SetAlpha( wdg_text, 255 );
-	
-#ifndef SKIP_BOOT_SCREEN
-	for( angle=0,alpha=0; alpha<250; alpha+=10, angle+=10 ) {
-		Widget_SetAlpha( wdg_ring, alpha );
-		Graph_Rotate( &img_ring, angle, &img_buff );
-		Widget_SetBackgroundImage( wdg_ring, &img_buff );
-		LCUI_MSleep(25);
-	}
-#endif
-	Widget_SetAlpha( wdg_ring, 255 );
-	LCUIThread_Create( &t, Game_LoadData, (void*)wdg_text );
-	while(1) {
-		Graph_Rotate( &img_ring, angle, &img_buff );
-		Widget_SetBackgroundImage( wdg_ring, &img_buff );
-		LCUI_MSleep(25);
-		angle+=10;
-		if( angle >= 360 ) {
-			angle = 0;
-		}
-		if( res_load_state == 1 ) {
-			break;
-		} else if( res_load_state == -1 ) {
-			return -1;
-		}
-	}
-#ifndef SKIP_BOOT_SCREEN
-	for( alpha=250; alpha>10; alpha-=10 ) {
-		Widget_SetAlpha( wdg_ring, alpha );
-		Graph_Rotate( &img_ring, angle, &img_buff );
-		Widget_SetBackgroundImage( wdg_ring, &img_buff );
-		if( angle >= 360 ) {
-			angle = 0;
-		}
-		angle+=10;
-		if( angle >= 360 ) {
-			angle = 0;
-		}
-		LCUI_MSleep(25);
-	}
-#endif
-	Widget_SetAlpha( wdg_ring, 0 );
-	Widget_Hide( wdg_ring );
-#ifndef SKIP_BOOT_SCREEN
-	/* 以淡出的效果显示LOGO */
-	for( alpha=250; alpha>10; alpha-=20 ) {
-		Widget_SetAlpha( wdg_logo, alpha );
-		Widget_SetAlpha( wdg_text, alpha );
-		LCUI_MSleep(25);
-	}
-#endif
-	Widget_SetAlpha( wdg_logo, 0 );
-	Widget_SetAlpha( wdg_text, 0 );
-	Widget_Hide( wdg_logo );
-	Widget_Hide( wdg_text );
-	Widget_Destroy( wdg_text );
-	Widget_Destroy( wdg_logo );
-	return 0;
+	TextStyle_FontSize( &style, 14 );
+	Label_TextStyle( widget, style );
+	Widget_SetAlign( widget, ALIGN_TOP_RIGHT, Pos(-20,20) );
+	LCUITimer_Set( 500, UpdateViewFPS, widget, TRUE );
+	Widget_Show( widget );
 }
 
 static void Game_MainThread( void *arg )
 {
 	int ret;
+	ret = GameGraphRes_LoadFromFile("main.data");
+	if( ret != 0 ) {
+		LCUI_MessageBoxW(
+			MB_ICON_ERROR,
+			L"游戏资源载入出错，请检查程序的完整性！",
+			L"错误", MB_BTN_OK );
+		LCUIThread_Exit(NULL);
+		return;
+	}
 	LCUICursor_Hide();		/* 隐藏鼠标游标 */
 	Game_ShowBootScreen();		/* 显示启动画面 */
 	LCUICursor_Show();		/* 显示鼠标游标 */
-	ret = Game_ShowLoadingScreen();	/* 显示载入画面 */
-	if( ret == 0 ) {
-		//Game_InitLANBattle();
-		//Game_InitMainMenu();	/* 初始化游戏主菜单 */
-		//Game_ShowMainMenu();	/* 显示游戏主菜单 */
+	InitViewFPS();
+	ret = Game_Init();
+	if( ret != 0 ) {
+		LCUI_MainLoop_Quit(NULL);
+		LCUIThread_Exit(NULL);
 	}
+	Game_Start();
+	Game_Loop();
 	LCUIThread_Exit(NULL);
 }
 
@@ -235,38 +123,18 @@ static void InitConsoleWindow(void)
 }
 #endif
 
-static void UpdateViewFPS( void *arg )
-{
-	char str[10];
-	LCUI_Widget *label = (LCUI_Widget*)arg;
-	sprintf( str, "FPS: %d", LCUIScreen_GetFPS() );
-	Label_Text( label, str );
-}
-
 int main( int argc, char **argv )
 {
 	LCUI_Thread t;
-	LCUI_Widget *widget;
-	LCUI_TextStyle style;
 	
 #ifdef LCUI_BUILD_IN_WIN32
-	InitConsoleWindow();
+	//InitConsoleWindow();
 #endif
 	LCUI_Init(640,480,0);
 	
-	/* 预先载入图像资源 */
-	GraphRes_Load();
-	widget = Widget_New("label");
-	TextStyle_Init( &style );
-	TextStyle_FontSize( &style, 14 );
-	Label_TextStyle( widget, style );
-	Widget_SetAlign( widget, ALIGN_TOP_RIGHT, Pos(-20,20) );
-	LCUITimer_Set( 500, UpdateViewFPS, widget, TRUE );
-	Widget_Show( widget );
-
+	/* 初始化游戏资源 */
+	GameGraphRes_Init();
 	/* 创建一个线程，用于进行游戏的初始化 */
-	//LCUIThread_Create( &t, Game_MainThread, NULL );
-	Game_Init();
-	Game_Start();
+	LCUIThread_Create( &t, Game_MainThread, NULL );
 	return LCUI_Main();
 }
