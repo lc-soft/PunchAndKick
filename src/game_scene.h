5d3cb9af54f4245dd1dcdd6429b0b17b1dee8cab
Liu Chao <lc-soft@live.cn>
2013-08-08 21:26:15 +0800

    加入战斗场景

---
+++ src/game_scene.h
@@ -0,0 +1,18 @@
+﻿
+/** 初始化战斗场景 */
+int GameScene_Init( LCUI_Widget *scene );
+
+/** 获取战斗场景的地面尺寸 */
+void GameScene_GetLandSize( LCUI_Widget *scene, LCUI_Size *size );
+
+/** 获取空间地面在场景中的起点Y轴坐标 */
+void GameScene_GetLandStartY( LCUI_Widget *scene, int *y );
+
+/** 获取空间地面在场景中的起点X轴坐标 */
+void GameScene_GetLandStartX( LCUI_Widget *scene, int *x );
+
+/** 更新场景上的镜头位置，使目标游戏对象处于镜头区域内 */
+int GameScene_UpdateCamera( LCUI_Widget *scene, LCUI_Widget *target );
+
+/** 退出战斗场景，释放占用的内存资源 */
+int GameScene_Quit( LCUI_Widget *scene );
