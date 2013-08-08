bb8c5ad96bd50e3ea419af2842129aed31b5077b
Liu Chao <lc-soft@live.cn>
2013-08-08 21:31:42 +0800

    更新头文件

--- src/game.h
+++ src/game.h
@@ -5,6 +5,8 @@ LCUI_BEGIN_HEADER
 #include "game_control.h"
 #include "game_action.h"
 #include "game_resource.h"
+#include "game_space.h"
+#include "game_scene.h"
 #include "game_msg.h"
 #include "main_menu.h"
 
