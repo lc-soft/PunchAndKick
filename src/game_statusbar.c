5cbe7eb894f40b863c6f7058be6695e4c2ca7893
Liu Chao <lc-soft@live.cn>
2013-08-11 08:53:09 +0800

    添加状态栏部件

---
+++ src/game_statusbar.c
@@ -0,0 +1,151 @@
+﻿#include <LCUI_Build.h>
+#include LC_LCUI_H
+#include LC_WIDGET_H
+#include LC_LABEL_H
+#include LC_PROGBAR_H
+#include LC_CHARSET_H
+#include "game_lifebar.h"
+
+#define TYPE_NAME	"GamePlayerStatusBar"
+
+typedef struct StatusBarData_ {
+	int hp;				/**< 当前生命值 */
+	int max_hp;			/**< 最大生命值 */
+	wchar_t player_name[9];		/**< 玩家名 */
+	wchar_t type_name[5];		/**< 类型名 */
+	LCUI_Graph *avatar;		/**< 头像 */
+	LCUI_Widget *img_box;		/**< 头像框部件 */
+	LCUI_Widget *lifebar;		/**< 血量条部件 */
+	LCUI_Widget *label_type;	/**< 显示类型名的部件 */
+	LCUI_Widget *label_type_area;	/**< 一个容器 */
+	LCUI_Widget *label_name;	/**< 显示玩家名的部件 */
+} StatusBarData;
+
+LCUI_Widget *StatusBar_New(void)
+{
+	LCUI_Widget *widget;
+	widget = Widget_New(TYPE_NAME);
+	Widget_Resize( widget, Size(195,80) );
+	return widget;
+}
+
+static void StatusBar_ExecInit( LCUI_Widget *widget )
+{
+	StatusBarData *data;
+	LCUI_TextStyle style;
+
+	data = (StatusBarData*)Widget_NewPrivData( widget, sizeof(StatusBarData));
+	data->avatar = NULL;
+	data->hp = 250;
+	data->max_hp = 400;
+	wcsncpy( data->player_name, L"匿名", 8 );
+	data->player_name[8] = 0;
+	wcsncpy( data->type_name, L"NONE", 8 );
+	data->player_name[5] = 0;
+	/* 创建部件 */
+	data->img_box = Widget_New(NULL);
+	data->label_type = Widget_New("label");
+	data->label_name = Widget_New("label");
+	data->label_type_area = Widget_New(NULL);
+	data->lifebar = LifeBar_New();
+	/* 加入至本容器 */
+	Widget_Container_Add( widget, data->img_box );
+	Widget_Container_Add( widget, data->label_type_area );
+	Widget_Container_Add( widget, data->label_name );
+	Widget_Container_Add( widget, data->lifebar );
+	Widget_Container_Add( data->label_type_area, data->label_type );
+	/* 设置默认的文本样式 */
+	TextStyle_Init( &style );
+	TextStyle_FontSize( &style, 14 );
+	Label_TextStyle( data->label_type, style );
+	Label_TextStyle( data->label_name, style );
+	/* 调整尺寸 */
+	Widget_Resize( data->label_type_area, Size(45,15));
+	Widget_Resize( data->img_box, Size(45,45) );
+	Widget_Resize( data->lifebar, Size(135,25) );
+	/* 加上边框 */
+	Widget_SetBorder( data->img_box, Border(1,BORDER_STYLE_SOLID,RGB(150,150,150)) );
+	Widget_SetBorder( widget, Border(1,BORDER_STYLE_SOLID,RGB(150,150,150)) );
+	/* 设置布局 */
+	Widget_SetAlign( data->img_box, ALIGN_TOP_LEFT, Pos(5,25) );
+	Widget_SetAlign( data->label_type, ALIGN_MIDDLE_CENTER, Pos(0,0) );
+	Widget_SetAlign( data->label_type_area, ALIGN_TOP_LEFT, Pos(5,5) );
+	Widget_SetAlign( data->label_name, ALIGN_TOP_LEFT, Pos(55+2,5) );
+	Widget_SetAlign( data->lifebar, ALIGN_TOP_LEFT, Pos(55,25) );
+	
+	Widget_SetBackgroundColor( data->img_box, RGB(240,240,240) );
+	Widget_SetBackgroundTransparent( data->img_box, FALSE );
+	Widget_SetBackgroundColor( widget, RGB(255,255,255) );
+	Widget_SetBackgroundTransparent( widget, FALSE );
+
+	/* 设置生命条的参数 */
+	LifeBar_SetFullHP( data->lifebar, 100 );
+	LifeBar_SetMaxHP( data->lifebar, data->max_hp );
+	LifeBar_SetHP( data->lifebar, data->hp );
+	/* 设置显示的文本 */
+	Label_TextW( data->label_type, data->type_name );
+	Label_TextW( data->label_name, data->player_name );
+	/* 全部显示 */
+	Widget_Show( data->label_type );
+	Widget_Show( data->label_type_area );
+	Widget_Show( data->label_name );
+	Widget_Show( data->img_box );
+	Widget_Show( data->lifebar );
+}
+
+/** 设置玩家名称(最多8个字) */
+void StatusBar_SetPlayerNameW( LCUI_Widget *widget, const wchar_t *name )
+{
+	StatusBarData *data;
+	data = (StatusBarData*)Widget_GetPrivData( widget );
+	wcsncpy( data->player_name, name, 8 );
+	Label_TextW( data->label_name, data->player_name );
+}
+
+void StatusBar_SetPlayerName( LCUI_Widget *widget, const char *name )
+{
+
+}
+
+/** 设置玩家类型名称，最多4个字，如：1P、2P或CPU */
+void StatusBar_SetPlayerTypeNameW( LCUI_Widget *widget, const wchar_t *type_name )
+{
+	StatusBarData *data;
+	data = (StatusBarData*)Widget_GetPrivData( widget );
+	wcsncpy( data->type_name, type_name, 4 );
+	Label_TextW( data->label_type, data->type_name );
+}
+
+void StatusBar_SetPlayerTypeName( LCUI_Widget *widget, const char *type_name )
+{
+
+}
+
+/** 设置最大生命值 */
+void StatusBar_SetMaxHealth( LCUI_Widget *widget, int value )
+{
+	StatusBarData *data;
+	data = (StatusBarData*)Widget_GetPrivData( widget );
+	LifeBar_SetMaxHP( data->lifebar, value );
+}
+
+/** 设置当前生命值 */
+void StatusBar_SetHealth( LCUI_Widget *widget, int value )
+{
+	StatusBarData *data;
+	data = (StatusBarData*)Widget_GetPrivData( widget );
+	LifeBar_SetHP( data->lifebar, value );
+}
+
+/** 设置头像 */
+void StatusBar_SetAvatar( LCUI_Widget *widget, LCUI_Graph *img )
+{
+
+}
+
+/** 注册状态栏部件 */
+void StatusBar_Register(void)
+{
+	WidgetType_Add(TYPE_NAME);
+	WidgetFunc_Add(TYPE_NAME, StatusBar_ExecInit, FUNC_TYPE_INIT );
+}
---
+++ src/game_statusbar.h
@@ -0,0 +1,28 @@
+﻿#ifndef __GAME_STATUSBAR_H__
+#define __GAME_STATUSBAR_H__
+
+LCUI_Widget *StatusBar_New(void);
+
+/** 设置玩家名称(最多8个字) */
+void StatusBar_SetPlayerNameW( LCUI_Widget *widget, const wchar_t *name );
+
+void StatusBar_SetPlayerName( LCUI_Widget *widget, const char *name );
+
+/** 设置玩家类型名称，最多4个字，如：1P、2P或CPU */
+void StatusBar_SetPlayerTypeNameW( LCUI_Widget *widget, const wchar_t *type_name );
+
+void StatusBar_SetPlayerTypeName( LCUI_Widget *widget, const char *type_name );
+
+/** 设置最大生命值 */
+void StatusBar_SetMaxHealth( LCUI_Widget *widget, int value );
+
+/** 设置当前生命值 */
+void StatusBar_SetHealth( LCUI_Widget *widget, int value );
+
+/** 设置头像 */
+void StatusBar_SetAvatar( LCUI_Widget *widget, LCUI_Graph *img );
+
+/** 注册状态栏部件 */
+void StatusBar_Register(void);
+
+#endif
