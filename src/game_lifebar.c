0fb25f42a68e3606d8bba36be340443c9770b692
Liu Chao <lc-soft@live.cn>
2013-08-13 20:11:47 +0800

    实现血条在扣血时的血条残影的动态变化效果

--- src/game_lifebar.c
+++ src/game_lifebar.c
@@ -4,18 +4,26 @@
 #include LC_LCUI_H
 #include LC_WIDGET_H
 
+#define SHADOW_N_WAIT	30
+
+#define SHADOW_STATE_CLEAN		1
+#define SHADOW_STATE_END_CLEAN		0
+#define SHADOW_STATE_NEED_CLEAN		2
+
 typedef struct LifeBarData_ {
 	int current_life_point;		/**< 当前生命点 */
-	int old_life_point;		/**< 上次的生命点 */
+	int old_life_point;		/**< 扣血前的生命点 */
 	int full_life_point;		/**< 满血条的生命点数 */
 	int max_life_point;		/**< 能够拥有的最大生命点 */
-
-	int flashbar_timer;		/**< 闪光条的定时器 */
+	int shadow_life_point;		/**< 血条残影表示的血量 */
+	int timer_flashbar;		/**< 闪光条的定时器 */
+	int timer_lifebar_shadow;	/**< 生命条残影的定时器 */
+	int n_erase_wait;		/**< 抹去生命条残影前需要等待的次数 */
 	LCUI_BOOL need_show_flash;	/**< 指示是否需要显示闪光 */
-
+	int lifebar_shadow_state;	/**< 生命条残影的状态 */
 	LCUI_Widget *lifebar;		/**< 当前生命条 */
 	LCUI_Widget *flashbar[2];	/**< 扣血时显示的闪光条 */
-	LCUI_Widget *oldlifebar;	/**< 扣血前显示的生命条 */
+	LCUI_Widget *lifebar_shadow[3];	/**< 生命条残影 */
 } LifeBarData;
 
 static LCUI_RGB color_set[5]={
@@ -28,6 +36,146 @@ static LCUI_RGB color_set[5]={
 
 #define TYPE_NAME	"GamePlayerLifeBar"
 
+/** 计算生命条的数量 */
+static int LifeBar_CountBarNum( int cur_hp, int full_hp )
+{
+	int n;
+	if( cur_hp <= full_hp ) {
+		return 1;
+	}
+	n = cur_hp / full_hp;
+	if( cur_hp % full_hp == 0 ) {
+		return n;
+	}
+	return n+1;
+}
+
+static void LifeBar_GetShadowForeColor( int n_lifebar, LCUI_RGB *color )
+{
+	if( n_lifebar > 0 ) {
+		n_lifebar -= 1;
+	}
+	color->red = color_set[n_lifebar%5].red*4/5;
+	color->green = color_set[n_lifebar%5].green*4/5;
+	color->blue = color_set[n_lifebar%5].blue*4/5;
+}
+
+static void LifeBar_GetShadowBackColor( int n_lifebar, LCUI_RGB *color )
+{
+	int n;
+	if( n_lifebar > 0 ) {
+		n_lifebar -= 1;
+	}
+	n = n_lifebar%5 - 1;
+	if( n<0 ) {
+		n = 4;
+	}
+	color->red = color_set[n].red*4/5;
+	color->green = color_set[n].green*4/5;
+	color->blue = color_set[n].blue*4/5;
+}
+
+static void LifeBar_UpdateShadow( void *arg )
+{
+	LifeBarData *data;
+	double scale;
+	int cur_n, old_n;
+	char scale_str[10];
+	LCUI_RGB fore_color, back_color;
+
+	data = (LifeBarData*)Widget_GetPrivData((LCUI_Widget*)arg);
+	if( data->lifebar_shadow_state == SHADOW_STATE_END_CLEAN ) {
+		return;
+	}
+
+	if( data->lifebar_shadow_state == SHADOW_STATE_CLEAN ) {
+		/* 如果还需要等几次 */
+		if( data->n_erase_wait > 0 ) {
+			--data->n_erase_wait;
+			return;
+		} else {
+			data->n_erase_wait = 0;
+		}
+	}
+
+	if( data->lifebar_shadow_state == SHADOW_STATE_NEED_CLEAN ) {
+		data->lifebar_shadow_state = SHADOW_STATE_CLEAN;
+	}
+	cur_n = LifeBar_CountBarNum( data->current_life_point, data->full_life_point );
+	old_n = LifeBar_CountBarNum( data->shadow_life_point, data->full_life_point );
+	if( old_n < cur_n ) {
+		return;
+	}
+	else if( old_n == cur_n ) {
+		LifeBar_GetShadowForeColor( cur_n, &fore_color );
+		LifeBar_GetShadowBackColor( cur_n, &back_color );
+		scale = data->shadow_life_point % data->full_life_point;
+		if( scale == 0.0 ) {
+			scale = 100.0;
+		} else {
+			scale = 100 * scale / data->full_life_point;
+		}
+		Widget_SetBackgroundColor( data->lifebar_shadow[2], fore_color );
+		Widget_SetBackgroundTransparent( data->lifebar_shadow[2], FALSE );
+		sprintf( scale_str, "%.2lf%%", scale );
+		Widget_SetSize( data->lifebar_shadow[2], scale_str, "100%" );
+		Widget_Show( data->lifebar_shadow[2] );
+		Widget_Hide( data->lifebar_shadow[1] );
+		Widget_Hide( data->lifebar_shadow[0] );
+	}
+	else if( old_n - cur_n == 1 ) {
+		LifeBar_GetShadowForeColor( old_n, &fore_color );
+		LifeBar_GetShadowForeColor( cur_n, &back_color );
+		scale = data->shadow_life_point % data->full_life_point;
+		if( scale == 0.0 ) {
+			scale = 100.0;
+		} else {
+			scale = 100 * scale / data->full_life_point;
+		}
+		Widget_SetBackgroundColor( data->lifebar_shadow[1], fore_color );
+		Widget_SetBackgroundColor( data->lifebar_shadow[2], back_color );
+		Widget_SetBackgroundTransparent( data->lifebar_shadow[2], FALSE );
+		Widget_SetBackgroundTransparent( data->lifebar_shadow[1], FALSE );
+		sprintf( scale_str, "%.2lf%%", scale );
+		Widget_SetSize( data->lifebar_shadow[1], scale_str, "100%" );
+		Widget_SetSize( data->lifebar_shadow[2], "100%", "100%" );
+		Widget_Show( data->lifebar_shadow[2] );
+		Widget_Show( data->lifebar_shadow[1] );
+		Widget_Hide( data->lifebar_shadow[0] );
+	} else {
+		LifeBar_GetShadowForeColor( old_n, &fore_color );
+		LifeBar_GetShadowBackColor( old_n, &back_color );
+		scale = data->shadow_life_point % data->full_life_point;
+		if( scale == 0.0 ) {
+			scale = 100.0;
+		} else {
+			scale = 100 * scale / data->full_life_point;
+		}
+		Widget_SetBackgroundColor( data->lifebar_shadow[0], fore_color );
+		Widget_SetBackgroundColor( data->lifebar_shadow[1], back_color );
+		Widget_SetBackgroundTransparent( data->lifebar_shadow[2], FALSE );
+		Widget_SetBackgroundTransparent( data->lifebar_shadow[1], FALSE );
+		Widget_SetBackgroundTransparent( data->lifebar_shadow[0], FALSE );
+		sprintf( scale_str, "%.2lf%%", scale );
+		Widget_SetSize( data->lifebar_shadow[0], scale_str, "100%" );
+		Widget_SetSize( data->lifebar_shadow[1], "100%", "100%" );
+		Widget_SetSize( data->lifebar_shadow[2], "100%", "100%" );
+		Widget_Show( data->lifebar_shadow[2] );
+		Widget_Show( data->lifebar_shadow[1] );
+		Widget_Show( data->lifebar_shadow[0] );
+	}
+	/* 最少要扣1点，不然当data->full_life_point小于20时，血条残影就不会动了 */
+	data->shadow_life_point -= (data->full_life_point/20+1);
+	if( data->shadow_life_point <= data->current_life_point ) {
+		data->lifebar_shadow_state = SHADOW_STATE_END_CLEAN;
+		Widget_Hide( data->lifebar_shadow[2] );
+		Widget_Hide( data->lifebar_shadow[1] );
+		Widget_Hide( data->lifebar_shadow[0] );
+	}
+
+}
+
+/** 定时更新闪光条 */
 static void LifeBar_UpdateFlashBar( void *arg )
 {
 	double scale;
@@ -40,11 +188,10 @@ static void LifeBar_UpdateFlashBar( void *arg )
 	if( data->need_show_flash ) {
 		data->need_show_flash = FALSE;
 		if( data->old_life_point < data->current_life_point ) {
-			_DEBUG_MSG("tip\n");
 			return;
 		}
-		new_n = data->current_life_point/data->full_life_point;
-		old_n = data->old_life_point/data->full_life_point;
+		new_n = LifeBar_CountBarNum( data->current_life_point, data->full_life_point );
+		old_n = LifeBar_CountBarNum( data->old_life_point, data->full_life_point );
 		/* 如果扣血时减少了血条数 */
 		if( old_n > new_n ) {
 			/* 如果扣除的血量超过1条，并且之前刚好是满槽血 */
@@ -110,25 +257,35 @@ static void LifeBar_ExecInit( LCUI_Widget *widget )
 	data->max_life_point = 200;
 	data->old_life_point = 0;
 	data->need_show_flash = FALSE;
+	data->lifebar_shadow_state = SHADOW_STATE_END_CLEAN;
+	data->n_erase_wait = SHADOW_N_WAIT;
 	data->lifebar = Widget_New(NULL);
 	data->flashbar[0] = Widget_New(NULL);
 	data->flashbar[1] = Widget_New(NULL);
-	data->oldlifebar = Widget_New(NULL);
+	data->lifebar_shadow[0] = Widget_New(NULL);
+	data->lifebar_shadow[1] = Widget_New(NULL);
+	data->lifebar_shadow[2] = Widget_New(NULL);
 	/* 加入至容器 */
 	Widget_Container_Add( widget, data->lifebar );
 	Widget_Container_Add( widget, data->flashbar[0] );
 	Widget_Container_Add( widget, data->flashbar[1] );
-	Widget_Container_Add( widget, data->oldlifebar );
+	Widget_Container_Add( widget, data->lifebar_shadow[0] );
+	Widget_Container_Add( widget, data->lifebar_shadow[1] );
+	Widget_Container_Add( widget, data->lifebar_shadow[2] );
 	/* 调整堆叠顺序 */
-	Widget_SetZIndex( data->flashbar[0], 3 );
+	Widget_SetZIndex( data->flashbar[0], 5 );
+	Widget_SetZIndex( data->lifebar_shadow[0], 4 );
+	Widget_SetZIndex( data->lifebar_shadow[1], 3 );
 	Widget_SetZIndex( data->lifebar, 2 );
 	Widget_SetZIndex( data->flashbar[1], 1 );
-	Widget_SetZIndex( data->oldlifebar, 0 );
+	Widget_SetZIndex( data->lifebar_shadow[2], 0 );
 
 	Widget_SetBorder( widget, Border(2,BORDER_STYLE_SOLID,RGB(150,150,150)) );
 	Widget_SetPadding( widget, Padding(2,2,2,2) );
 	Widget_SetAlign( data->lifebar, ALIGN_MIDDLE_LEFT, Pos(0,0) );
-	Widget_SetAlign( data->oldlifebar, ALIGN_MIDDLE_LEFT, Pos(0,0) );
+	Widget_SetAlign( data->lifebar_shadow[0], ALIGN_MIDDLE_LEFT, Pos(0,0) );
+	Widget_SetAlign( data->lifebar_shadow[1], ALIGN_MIDDLE_LEFT, Pos(0,0) );
+	Widget_SetAlign( data->lifebar_shadow[2], ALIGN_MIDDLE_LEFT, Pos(0,0) );
 	Widget_SetAlign( data->flashbar[0], ALIGN_MIDDLE_LEFT, Pos(0,0) );
 	Widget_SetAlign( data->flashbar[1], ALIGN_MIDDLE_LEFT, Pos(0,0) );
 	Widget_SetBackgroundTransparent( data->lifebar, FALSE );
@@ -140,7 +297,8 @@ static void LifeBar_ExecInit( LCUI_Widget *widget )
 	Widget_SetBackgroundColor( data->lifebar, color_set[0] );
 	Widget_Show( data->lifebar );
 	/* 创建一个定时器，实现闪光条的淡出效果 */
-	data->flashbar_timer = LCUITimer_Set( 20, LifeBar_UpdateFlashBar, (void*)widget, TRUE );
+	data->timer_flashbar = LCUITimer_Set( 20, LifeBar_UpdateFlashBar, (void*)widget, TRUE );
+	data->timer_lifebar_shadow = LCUITimer_Set( 20, LifeBar_UpdateShadow, (void*)widget, TRUE );
 }
 
 static void LifeBar_ExecUpdate( LCUI_Widget *widget )
@@ -238,6 +396,13 @@ void LifeBar_SetHP( LCUI_Widget *widget, int hp )
 	/* 如果是扣血，那就记录扣血前的血量，并标记需要显示闪光 */
 	if( hp < data->old_life_point ) {
 		data->need_show_flash = TRUE;
+		/* 如果不需要抹去血条残影，则说明可以显示血条残影 */
+		if( data->lifebar_shadow_state == SHADOW_STATE_END_CLEAN ) {
+			/* 记录本次扣血前的血量，并标记需要抹去血条残影 */
+			data->shadow_life_point = data->old_life_point;
+			data->n_erase_wait = SHADOW_N_WAIT;
+			data->lifebar_shadow_state = SHADOW_STATE_NEED_CLEAN;
+		}
 	}
 	/* 血条的长度需要更新 */
 	Widget_Update( widget );
@@ -259,16 +424,10 @@ void LifeBar_SetMaxHP( LCUI_Widget *widget, int hp )
 /** 获取生命条的数量 */
 int LifeBar_GetBarNum( LCUI_Widget *widget )
 {
-	int n;
 	LifeBarData *data;
 
 	data = (LifeBarData *)Widget_GetPrivData(widget);
-	if( data->current_life_point <= data->full_life_point ) {
-		n = 0;
-	} else {
-		n = data->current_life_point / data->full_life_point;
-	}
-	return n+1;
+	return LifeBar_CountBarNum( data->current_life_point, data->full_life_point );
 }
 
 /** 注册生命条部件 */
