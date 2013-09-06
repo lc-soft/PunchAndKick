b71d5c5e7e639b45cbc44c2a8a08ecac302472e3
Liu Chao <lc-soft@live.cn>
2013-09-07 00:28:56 +0800

    添加头文件

---
+++ src/skills/skills_martial_artist.h
@@ -0,0 +1,14 @@
+﻿
+#ifndef __MARTIAL_ARTIST_SKILL_H__
+#define __MARTIAL_ARTIST_SKILL_H__
+
+#define SKILLNAME_ELBOW		"elbow"
+#define SKILLNAME_KNEEHIT	"knee hit"
+/** 定义这两个技能的优先级 */
+#define SKILLPRIORITY_ELBOW	SKILLPRIORITY_MACH_A_ATTACK
+#define SKILLPRIORITY_KNEEHIT	SKILLPRIORITY_ELBOW
+
+/** 注册拳击家特有的技能 */
+void MartialArtistSkill_Register(void);
+
+#endif
