c23a91739d17abbd410c8e52a6a3d513d3037da3
Liu Chao <lc-soft@live.cn>
2013-09-07 20:22:56 +0800

    调整攻击处理

--- src/game_attack.c
+++ src/game_attack.c
@@ -5,24 +5,72 @@
 #include LC_WIDGET_H
 #include "game.h"
 
+#define NAME_MAX_LEN	64
+
 typedef struct AttackRecord_ {
-	GamePlayer *attacker;	/**< 攻击者 */
-	int attack_type;	/**< 攻击类型 */
-	GamePlayer *victim;	/**< 受害者 */
-	int victim_state;	/**< 受害者的状态 */
+	GamePlayer *attacker;			/**< 攻击者 */
+	GamePlayer *victim;			/**< 受害者 */
+	char attack_type_name[NAME_MAX_LEN];	/**< 攻击类型 */
+	int victim_state;			/**< 受害者的状态 */
 } AttackRecord;
 
+typedef struct AttackInfo_ {
+	int attack_type_id;			/**< 攻击标识符 */
+	char attack_type_name[NAME_MAX_LEN];	/**< 攻击名称 */
+	int (*get_damage)(GamePlayer*);		/**< 获取实际伤害值 */
+	void (*set_action)(GamePlayer*);	/**< 设置受攻击者的动作 */
+} AttackInfo;
+
 static LCUI_Queue attack_record;
+static LCUI_Queue attack_library;
+
+void AttackLibrary_Init(void)
+{
+	Queue_Init( &attack_library, sizeof(AttackInfo), NULL );
+}
+
+static AttackInfo *AttackLibrary_GetInfo( const char *attack_type_name )
+{
+	AttackInfo *p_info;
+	int n, attack_type_id;
+	attack_type_id = (int)BKDRHash( attack_type_name );
+	n = Queue_GetTotal( &attack_library );
+	while(n--) {
+		p_info = (AttackInfo*)Queue_Get( &attack_library, n );
+		if( !p_info ) {
+			continue;
+		}
+		if( p_info->attack_type_id == attack_type_id ) {
+			return p_info;
+		}
+	}
+	return FALSE;
+}
+
+/**< 添加攻击数据 */
+int AttackLibrary_AddAttack(	const char *attack_name, 
+				int (*get_damage)(GamePlayer*),
+				void (*set_action)(GamePlayer*) )
+{
+	AttackInfo data;
+	strncpy( data.attack_type_name, attack_name, NAME_MAX_LEN );
+	data.attack_type_name[NAME_MAX_LEN-1] = 0;
+	data.attack_type_id = (int)BKDRHash( data.attack_type_name );
+	data.get_damage = get_damage;
+	data.set_action = set_action;
+	return data.attack_type_id;
+}
 
 /** 记录攻击者对受害者的攻击 */
 void Game_RecordAttack( GamePlayer *attacker,
-			int attack_type,
+			const char *attack_type_name,
 			GamePlayer *victim,
 			int victim_state )
 {
 	AttackRecord data;
 	data.attacker = attacker;
-	data.attack_type = attack_type;
+	strncpy( data.attack_type_name, attack_type_name, NAME_MAX_LEN );
+	data.attack_type_name[NAME_MAX_LEN-1] = 0;
 	data.victim = victim;
 	data.victim_state = victim_state;
 	Queue_Lock( &attack_record );
@@ -33,130 +81,33 @@ void Game_RecordAttack( GamePlayer *attacker,
 /** 处理已经记录的攻击 */
 void Game_ProcAttack(void)
 {
-	int n;
-	double damage, true_damage, reduce;
+	int n, true_damage;
 	AttackRecord *p_data;
+	AttackInfo *p_info;
 
 	Queue_Lock( &attack_record );
 	n = Queue_GetTotal( &attack_record );
 	for( ; n>=0; --n) {
 		p_data = (AttackRecord*)Queue_Get( &attack_record, 0 );
-		if( p_data == NULL ) {
+		if( !p_data || !p_data->victim ) {
 			continue;
 		}
-		/* 根据攻击者的攻击类型，计算输出伤害 */
-		switch(p_data->attack_type) {
-		case ATTACK_TYPE_JUMP_PUNCH:
-		case ATTACK_TYPE_PUNCH:
-			damage = p_data->attacker->property.punch/10.0;
-			break;
-		case ATTACK_TYPE_JUMP_KICK:
-		case ATTACK_TYPE_KICK:
-			damage = p_data->attacker->property.kick/10.0;
-			break;
-		case ATTACK_TYPE_MACH_STOMP:
-		case ATTACK_TYPE_JUMP_TREAD:
-			damage = p_data->attacker->property.kick/6.0;
-			break;
-		case ATTACK_TYPE_JUMP_ELBOW:
-			damage = p_data->attacker->property.punch/6.0;
-			break;
-		case ATTACK_TYPE_S_PUNCH:
-			damage = p_data->attacker->property.punch/3.0;
-			break;
-		case ATTACK_TYPE_S_KICK:
-			damage = p_data->attacker->property.kick/3.0;
-			break;
-		case ATTACK_TYPE_SJUMP_KICK:
-			damage = p_data->attacker->property.kick/2.0;
-			break;
-		case ATTACK_TYPE_SJUMP_PUNCH:
-			damage = p_data->attacker->property.punch/2.0;
-			break;
-		case ATTACK_TYPE_RIDE_ATTACK:
-			damage = p_data->attacker->property.punch/4.0;
-			break;
-		case ATTACK_TYPE_RIDE_JUMP_ATTACK:
-			damage = 10+p_data->attacker->property.kick/5.0;
-			break;
-		case ATTACK_TYPE_BIG_ELBOW:
-			damage = 20+p_data->attacker->property.punch/3.0;
-			break;
-		case ATTACK_TYPE_GUILLOTINE:
-			damage = 20+p_data->attacker->property.kick/3.0;
-			break;
-		case ATTACK_TYPE_SPIN_HIT:
-		case ATTACK_TYPE_SPIN_HIT2:
-			damage = 30 + p_data->attacker->property.kick/4.0;
-			damage += p_data->attacker->property.punch/4.0;
-			break;
-		case ATTACK_TYPE_BOMB_KICK:
-			damage = 50 + p_data->attacker->property.kick/4.0;
-			break;
-		case ATTACK_TYPE_JUMP_SPIN_KICK:
-			damage = 40 + p_data->attacker->property.kick/4.0;
-		case ATTACK_TYPE_FINAL_BLOW:
-			damage = p_data->attacker->property.punch/4.0;
-			damage += p_data->attacker->property.kick/4.0;
-			damage *= 2;
-			break;
-		case ATTACK_TYPE_THROW:
-			damage = 20 + p_data->attacker->property.throw/2.0;
-			break;
-		case ATTACK_TYPE_BUMPED:
-			damage = 20 + p_data->attacker->property.throw/4.0;
-			break;
-		case ATTACK_TYPE_ELBOW1:
-			damage = 20 + p_data->attacker->property.punch/5.0;
-			break;
-		case ATTACK_TYPE_ELBOW2:
-			damage = 100 + p_data->attacker->property.punch/3.0;
-			break;
-		case ATTACK_TYPE_KNEE_HIT1:
-			damage = p_data->attacker->property.punch/5.0;
-			damage += p_data->attacker->property.kick/5.0;
-			damage += p_data->victim->property.max_hp*0.01;
-			break;
-		case ATTACK_TYPE_KNEE_HIT2:
-			damage = 20 + p_data->victim->property.max_hp*0.02;
-			break;
-		case ATTACK_TYPE_NONE:
-		default:
-			damage = 0;
-			break;
+		/* 获取该类型攻击的信息 */
+		p_info = AttackLibrary_GetInfo( p_data->attack_type_name );
+		if( !p_info ) {
+			Queue_Delete( &attack_record, 0 );
+			continue;
 		}
-		/* 根据受害者的防御力，计算伤害的减免比例 */
-		reduce = p_data->victim->property.defense + 100;
-		reduce = 1 - 100 / reduce;
-		/* 根据受害者的状态，增加伤害减免比例 */
-		switch( p_data->victim_state ) {
-		case STATE_TUMMY:
-		case STATE_TUMMY_HIT:
-		case STATE_LYING:
-		case STATE_LYING_HIT:
-		case STATE_BE_LIFT_TUMMY:
-		case STATE_BE_LIFT_TUMMY_HIT:
-		case STATE_BE_LIFT_LYING:
-		case STATE_BE_LIFT_LYING_HIT:
-			/* 躺着或者趴着，减免20%的伤害 */
-			reduce += 0.20;
-			break;
-		case STATE_F_ROLL:
-		case STATE_B_ROLL:
-			reduce += 0.10;
-			break;
-		default:
-			break;
+		if( p_info->set_action ) {
+			/* 设置受攻击者的动作 */
+			p_info->set_action( p_data->victim );
 		}
-		/* 限制最大伤害减免比例为95% */
-		if( reduce > 0.95 ) {
-			reduce = 0.95;
+		if( p_info->get_damage ) {
+			/* 计算真实伤害 */
+			true_damage = p_info->get_damage( p_data->attacker );
 		}
-		/* 计算真实伤害 */
-		true_damage = damage * (1.00 - reduce) + 1;
-		DEBUG_MSG("true damage: %.2lf, damage: %.2lf, reduce: %.2lf%%\n", true_damage, damage, reduce*100 );
 		/* 计算现在的血量 */
-		p_data->victim->property.cur_hp -= (int)true_damage;
+		p_data->victim->property.cur_hp -= true_damage;
 		if( p_data->victim->property.cur_hp < 0 ) {
 			p_data->victim->property.cur_hp = 0;
 		}
@@ -172,5 +123,6 @@ void Game_ProcAttack(void)
 /** 初始化攻击记录 */
 void Game_InitAttackRecord(void)
 {
+	AttackLibrary_Init();
 	Queue_Init( &attack_record, sizeof(AttackRecord), NULL );
 }
--- src/game_attack.h
+++ src/game_attack.h
@@ -1,8 +1,35 @@
-﻿// 处理游戏角色的攻击伤害
+﻿// 处理游戏角色的攻击伤害，以及受攻击时的动作
+#ifndef __GAME_ATTACK_H__
+#define __GAME_ATTACK_H__
+
+#define ATK_NONE			"none"
+#define ATK_ELBOW_1			"elbow attack step 1"
+#define ATK_ELBOW_2			"elbow attack step 2"
+#define ATK_KNEE_HIT_1			"knee hit attack step 1"
+#define ATK_KNEE_HIT_2			"knee hit attack step 2"
+#define ATK_THROW			"throw attack"
+#define ATK_RIDE_A_ATTACK		"ride A attack"
+#define ATK_RIDE_B_ATTACK		"ride B attack"
+#define ATK_BUMPED			"bumped attack"
+#define ATK_FINALBLOW			"final blow attack"
+#define ATK_SPRINT_JUMP_A_ATTACK	"sprint jump A attack"
+#define ATK_SPRINT_JUMP_B_ATTACK	"sprint jump B attack"
+#define ATK_A_ATTACK			"A attack"
+#define ATK_B_ATTACK			"B attack"
+#define ATK_JUMP_TREAD			"jump tread attack"
+#define ATK_JUMP_ELBOW			"jump elbow attack"
+#define ATK_MACH_STOMP			"mach stomp attack"
+#define ATK_JUMP_A_ATTACK		"jump A attack"
+#define ATK_JUMP_B_ATTACK		"jump A attack"
+#define ATK_BOMBKICK			"bomb kick attack"
+#define ATK_SPINHIT			"spin hit attack"
+#define ATK_BIG_ELBOW			"big elbow attack"
+#define ATK_JUMP_SPINKICK		"jump spin kick attack"
+
 
 /** 记录攻击者对受害者的攻击 */
 void Game_RecordAttack( GamePlayer *attacker,
-			int attack_type,
+			const char *attack_type_name,
 			GamePlayer *victim,
 			int victim_state );
 
@@ -11,3 +38,5 @@ void Game_ProcAttack(void);
 
 /** 初始化攻击记录 */
 void Game_InitAttackRecord(void);
+
+#endif
