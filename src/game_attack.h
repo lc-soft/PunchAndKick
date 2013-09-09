975748f9fb671cee38785587c70ad58a1f3cbac9
Liu Chao <lc-soft@live.cn>
2013-09-09 23:11:09 +0800

    添加函数声明，添加攻击类型名称的宏定义

--- src/game_attack.h
+++ src/game_attack.h
@@ -3,10 +3,6 @@
 #define __GAME_ATTACK_H__
 
 #define ATK_NONE			"none"
-#define ATK_ELBOW_1			"elbow attack step 1"
-#define ATK_ELBOW_2			"elbow attack step 2"
-#define ATK_KNEE_HIT_1			"knee hit attack step 1"
-#define ATK_KNEE_HIT_2			"knee hit attack step 2"
 #define ATK_THROW			"throw attack"
 #define ATK_RIDE_A_ATTACK		"ride A attack"
 #define ATK_RIDE_B_ATTACK		"ride B attack"
@@ -16,16 +12,30 @@
 #define ATK_SPRINT_JUMP_B_ATTACK	"sprint jump B attack"
 #define ATK_A_ATTACK			"A attack"
 #define ATK_B_ATTACK			"B attack"
+#define ATK_SPRINT_A_ATTACK		"sprint A attack"
+#define ATK_SPRINT_B_ATTACK		"sprint B attack"
+#define ATK_MACH_A_ATTACK		"mach A attack"
+#define ATK_MACH_B_ATTACK		"mach B attack"
 #define ATK_JUMP_TREAD			"jump tread attack"
 #define ATK_JUMP_ELBOW			"jump elbow attack"
 #define ATK_MACH_STOMP			"mach stomp attack"
 #define ATK_JUMP_A_ATTACK		"jump A attack"
 #define ATK_JUMP_B_ATTACK		"jump A attack"
+#define ATK_JUMP_MACH_A_ATTACK		"jump mach A attack"
+#define ATK_JUMP_MACH_B_ATTACK		"jump mach A attack"
 #define ATK_BOMBKICK			"bomb kick attack"
 #define ATK_SPINHIT			"spin hit attack"
 #define ATK_BIG_ELBOW			"big elbow attack"
 #define ATK_JUMP_SPINKICK		"jump spin kick attack"
 
+
+/** 根据受害者的防御力及其它情况，获取减免后的实际伤害 */
+int DamageReduce( GamePlayer *victim, int victim_state, int demage );
+
+/**< 添加攻击数据 */
+int AttackLibrary_AddAttack(	const char *attack_name, 
+				int (*get_damage)(GamePlayer*,GamePlayer*,int),
+				void (*effect)(GamePlayer*,GamePlayer*) );
 
 /** 记录攻击者对受害者的攻击 */
 void Game_RecordAttack( GamePlayer *attacker,
