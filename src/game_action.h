23130e637cbb3893fb9ae7c56aa84d0e9577739a
Liu Chao <lc-soft@live.cn>
2013-09-22 21:05:12 +0800

    添加角色数据相关的函数

--- src/game_action.h
+++ src/game_action.h
@@ -1,11 +1,18 @@
-﻿
+﻿#ifndef __GAME_ACTION_H__
+#define __GAME_ACTION_H__
 enum RoleID {
-	ROLE_RIKI
+	ROLE_RIKI,
+	ROLE_TORAJI
 };
 
 #define GLOBAL_Y_WIDTH	12
 
-ActionData* ActionRes_LoadRiki( int action_type );
+int ActionRes_Riki_GetActionTotalNum(void);
+const ActionInfo *ActionRes_Riki_GetActionSet(void);
+
+int ActionRes_Toraji_GetActionTotalNum(void);
+const ActionInfo *ActionRes_Toraji_GetActionSet(void);
 
 /** 载入指定角色的动作动画 */
 ActionData* ActionRes_Load( int id, int action_type );
+#endif
