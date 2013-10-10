9140ae195f64610fc1771446e4b6aaa18e0f9ef1
Liu Chao <lc-soft@live.cn>
2013-10-10 22:26:32 +0800

    更新场景处理代码

--- src/game_scene.h
+++ src/game_scene.h
@@ -1,18 +1,24 @@
 ﻿
+/** 获取游戏场景 */
+LCUI_Widget *GetGameScene(void);
+
 /** 初始化战斗场景 */
-int GameScene_Init( LCUI_Widget *scene );
+int GameScene_Init( void );
 
 /** 获取战斗场景的地面尺寸 */
-void GameScene_GetLandSize( LCUI_Widget *scene, LCUI_Size *size );
-
-/** 获取空间地面在场景中的起点Y轴坐标 */
-void GameScene_GetLandStartY( LCUI_Widget *scene, int *y );
+void GameScene_GetLandSize( LCUI_Size *size );
 
 /** 获取空间地面在场景中的起点X轴坐标 */
-void GameScene_GetLandStartX( LCUI_Widget *scene, int *x );
+void GameScene_GetLandStartX( int *x );
+
+/** 获取空间地面在场景中的起点Y轴坐标 */
+void GameScene_GetLandStartY( int *y );
+
+/** 设置场景镜头焦点 */
+int GameScene_SetCameraTarget( LCUI_Widget *target );
 
 /** 更新场景上的镜头位置，使目标游戏对象处于镜头区域内 */
-int GameScene_UpdateCamera( LCUI_Widget *scene, LCUI_Widget *target );
+int GameScene_UpdateCamera( void );
 
 /** 退出战斗场景，释放占用的内存资源 */
-void GameScene_Quit( LCUI_Widget *scene );
+void GameScene_Quit( void );
