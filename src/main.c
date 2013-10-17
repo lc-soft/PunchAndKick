3ff08a64c724271a8cb570c21b4ad8c8367168fd
Liu Chao <lc-soft@live.cn>
2013-10-17 13:40:15 +0800

    添加一行中文版权声明，加上一点代码注释

--- src/main.c
+++ src/main.c
@@ -15,9 +15,10 @@
 #include "skills/game_skill.h"
 #include "game_role_select.h"
 
-#define TEXT_COPYRIGHT		L"Developed by LC-Games."
+#define TEXT_COPYRIGHT_EN	L"Developed by LC-Games, Copyright © 2013 LC-Games, All Rights Reserved."
+#define TEXT_COPYRIGHT_CN	L"本游戏由 LC-Games 开发 , LC-Games 保留所有权利。"
 #define TEXT_STATEMENT		L"<size=20px>"\
-				L"本程序仅用于展示：\n\n"\
+				L"此版本的游戏仅用于展示：\n\n"\
 				L" ● 游戏主菜单界面\n"\
 				L" ● 游戏角色的控制\n"\
 				L" ● 技能动作效果\n"\
@@ -26,25 +27,30 @@
 				L"游戏中大部分BUG是已知的，请无视本程序出现的一些细节上的BUG。\n"\
 				L"如果遇到较为严重的BUG，请关闭并重新运行本程序。</size>"
 
+/** 在屏幕上显示游戏启动时的画面 */
 static void Game_ShowBootScreen(void)
 {
 	uchar_t alpha;
 	LCUI_Graph img_game_logo;
-	LCUI_Widget *wdg_img_logo, *wdg_text, *wdg_copyright_text;
+	LCUI_Widget *wdg_img_logo, *wdg_text;
+	LCUI_Widget *wdg_copyright_cn_text, *wdg_copyright_en_text;
 
 	wdg_img_logo = Widget_New(NULL);
-	wdg_copyright_text = Widget_New("label");
+	wdg_copyright_cn_text = Widget_New("label");
+	wdg_copyright_en_text = Widget_New("label");
 	wdg_text = Widget_New("label");
 	
 	Graph_Init( &img_game_logo );
 	GameGraphRes_GetGraph(MAIN_RES, "main-logo", &img_game_logo );
 	
-	Label_TextW( wdg_copyright_text, TEXT_COPYRIGHT );
+	Label_TextW( wdg_copyright_en_text, TEXT_COPYRIGHT_EN );
+	Label_TextW( wdg_copyright_cn_text, TEXT_COPYRIGHT_CN );
 	Label_TextW( wdg_text, TEXT_STATEMENT );
 
 	Widget_Resize( wdg_img_logo, Graph_GetSize(&img_game_logo) );
 	
-	Widget_SetAlign( wdg_copyright_text, ALIGN_BOTTOM_CENTER, Pos(0,-20) );
+	Widget_SetAlign( wdg_copyright_cn_text, ALIGN_BOTTOM_CENTER, Pos(0,-20) );
+	Widget_SetAlign( wdg_copyright_en_text, ALIGN_BOTTOM_CENTER, Pos(0,-40) );
 	Widget_SetAlign( wdg_img_logo, ALIGN_MIDDLE_CENTER, Pos(0,0) );
 	Widget_SetAlign( wdg_text, ALIGN_MIDDLE_CENTER, Pos(0,0) );
 	
@@ -52,40 +58,47 @@ static void Game_ShowBootScreen(void)
 	Widget_SetBackgroundLayout( wdg_img_logo, LAYOUT_CENTER );
 	
 	Widget_SetAlpha( wdg_img_logo, 0 );
-	Widget_SetAlpha( wdg_copyright_text, 0 );
+	Widget_SetAlpha( wdg_copyright_cn_text, 0 );
+	Widget_SetAlpha( wdg_copyright_en_text, 0 );
 	Widget_SetAlpha( wdg_text, 0 );
 
 	Widget_Show( wdg_img_logo );
-	Widget_Show( wdg_copyright_text );
+	Widget_Show( wdg_copyright_en_text );
+	Widget_Show( wdg_copyright_cn_text );
 #ifndef SKIP_BOOT_SCREEN
 	/* 以淡入淡出的效果显示LOGO */
 	for( alpha=0; alpha<250; alpha+=10 ) {
 		Widget_SetAlpha( wdg_img_logo, alpha );
-		Widget_SetAlpha( wdg_copyright_text, alpha );
+		Widget_SetAlpha( wdg_copyright_en_text, alpha );
+		Widget_SetAlpha( wdg_copyright_cn_text, alpha );
 		LCUI_MSleep(25);
 	}
 	Widget_SetAlpha( wdg_img_logo, 255 );
-	Widget_SetAlpha( wdg_copyright_text, 255 );
+	Widget_SetAlpha( wdg_copyright_cn_text, 255 );
+	Widget_SetAlpha( wdg_copyright_en_text, 255 );
 	LCUI_Sleep(2);
 	for( alpha=255; alpha>5; alpha-=10 ) {
 		Widget_SetAlpha( wdg_img_logo, alpha );
-		Widget_SetAlpha( wdg_copyright_text, alpha );
+		Widget_SetAlpha( wdg_copyright_en_text, alpha );
+		Widget_SetAlpha( wdg_copyright_cn_text, alpha );
 		LCUI_MSleep(25);
 	}
 #endif
 	Widget_SetAlpha( wdg_img_logo, 0 );
-	Widget_SetAlpha( wdg_copyright_text, 0 );
+	Widget_SetAlpha( wdg_copyright_cn_text, 0 );
+	Widget_SetAlpha( wdg_copyright_en_text, 0 );
 	Widget_Hide( wdg_img_logo );
-	Widget_Hide( wdg_copyright_text );
+	Widget_Hide( wdg_copyright_en_text );
+	Widget_Hide( wdg_copyright_cn_text );
 	Widget_Show( wdg_text );
 #ifndef SKIP_BOOT_SCREEN
-	/* 以淡入淡出的效果显示LOGO */
+	/* 以淡入淡出的效果显示文本内容 */
 	for( alpha=0; alpha<250; alpha+=10 ) {
 		Widget_SetAlpha( wdg_text, alpha );
 		LCUI_MSleep(25);
 	}
 	Widget_SetAlpha( wdg_text, 255 );
-	LCUI_Sleep(4);
+	LCUI_Sleep(2);
 	for( alpha=255; alpha>5; alpha-=10 ) {
 		Widget_SetAlpha( wdg_text, alpha );
 		LCUI_MSleep(25);
@@ -94,9 +107,11 @@ static void Game_ShowBootScreen(void)
 	Widget_Hide( wdg_text );
 	Widget_Destroy( wdg_img_logo );
 	Widget_Destroy( wdg_text );
-	Widget_Destroy( wdg_copyright_text );
+	Widget_Destroy( wdg_copyright_en_text );
+	Widget_Destroy( wdg_copyright_cn_text );
 }
 
+/** 加入CPU玩家 */
 static void Game_AddCPUPlayer(void)
 {
 	int i, j, role_id;
@@ -125,23 +140,25 @@ static void Game_AddCPUPlayer(void)
 	}
 }
 
+/** 游戏线程 */
 static void Game_MainThread( void *arg )
 {
 	int role_id;
 	ControlKey ctrlkey;
 
-	srand((unsigned int)time(NULL));
-	GameObject_Register();
-	LCUICursor_Hide();		/* 隐藏鼠标游标 */
-	Game_ShowBootScreen();		/* 显示启动画面 */
-	LCUICursor_Show();		/* 显示鼠标游标 */
-	Game_InitMainMenu();
-	Game_ShowMainMenu();
-	Game_InitRoleSelectBox();
-	Game_ShowRoleSelectBox();
-	role_id = Game_GetSelectedRole();
-	Game_HideMainMenu();
-
+	srand((unsigned int)time(NULL));	/* 初始化本线程的随机种子 */
+	GameObject_Register();			/* 预先注册GameObject部件 */
+	LCUICursor_Hide();			/* 隐藏鼠标游标 */
+	Game_ShowBootScreen();			/* 显示启动画面 */
+	LCUICursor_Show();			/* 显示鼠标游标 */
+	Game_InitMainMenu();			/* 初始化主菜单界面 */
+	Game_ShowMainMenu();			/* 显示主菜单界面 */
+	Game_InitRoleSelectBox();		/* 初始化角色选择框 */
+	Game_ShowRoleSelectBox();		/* 显示角色选择框 */
+	role_id = Game_GetSelectedRole();	/* 获取选择的角色的ID */
+	Game_HideMainMenu();			/* 隐藏主菜单界面 */
+	
+	/* 设置1号玩家的控制键 */
 	ctrlkey.a_attack = LCUIKEY_J;
 	ctrlkey.b_attack = LCUIKEY_K;
 	ctrlkey.jump = LCUIKEY_SPACE;
@@ -151,13 +168,13 @@ static void Game_MainThread( void *arg )
 	ctrlkey.up = LCUIKEY_W;
 	ctrlkey.down = LCUIKEY_S;
 
-	Game_InitBattle();
-	Game_EnableGamePlayer( 1 );
-	Game_SetGamePlayer( 1, role_id, TRUE );
-	Game_SetGamePlayerControlKey( 1, &ctrlkey );
-	Game_AddCPUPlayer();
-	Game_StartBattle();
-	Game_Loop();
+	Game_InitBattle();				/* 初始化对战 */
+	Game_EnableGamePlayer( 1 );			/* 启用1号玩家 */
+	Game_SetGamePlayer( 1, role_id, TRUE );		/* 设置1号玩家信息 */
+	Game_SetGamePlayerControlKey( 1, &ctrlkey );	/* 设置1号玩家的控制键 */
+	Game_AddCPUPlayer();				/* 补全剩余玩家信息 */
+	Game_StartBattle();				/* 开始对战 */
+	Game_Loop();					/* 进入游戏主循环 */
 
 	LCUIThread_Exit(NULL);
 }
@@ -181,18 +198,9 @@ static void InitConsoleWindow(void)
 }
 #endif
 
-int main( int argc, char **argv )
+static int Game_LoadResource(void)
 {
 	int ret;
-	LCUI_Thread t;
-//#define DEBUG
-#if defined (LCUI_BUILD_IN_WIN32) && defined (DEBUG)
-	InitConsoleWindow();
-#endif
-	LCUI_Init(800,600,0);
-
-	/* 初始化游戏资源 */
-	GameGraphRes_Init();
 	ret = GameGraphRes_LoadFromFile("main.data");
 	if( ret != 0 ) {
 		LCUI_MessageBoxW(
@@ -207,7 +215,7 @@ int main( int argc, char **argv )
 			MB_ICON_ERROR,
 			L"字体资源载入出错，请检查程序的完整性！",
 			L"错误", MB_BTN_OK );
-		return -1;
+		return -2;
 	}
 	ret = GameGraphRes_LoadFromFile("action-riki.data");
 	ret |= GameGraphRes_LoadFromFile("action-kuni.data");
@@ -219,9 +227,25 @@ int main( int argc, char **argv )
 			MB_ICON_ERROR,
 			L"角色资源载入出错，请检查程序的完整性！",
 			L"错误", MB_BTN_OK );
-		return -2;
+		return -3;
 	}
-	/* 创建一个线程，用于进行游戏的初始化 */
+	return 0;
+}
+
+int main( int argc, char **argv )
+{
+	int ret;
+	LCUI_Thread t;
+//#define DEBUG
+#if defined (LCUI_BUILD_IN_WIN32) && defined (DEBUG)
+	InitConsoleWindow();
+#endif
+	LCUI_Init(800,600,0);		/* 初始化LCUI */
+	GameGraphRes_Init();		/* 初始化游戏资源 */
+	ret = Game_LoadResource();	/* 载入游戏资源 */
+	if( ret != 0 ) {
+		return -1;
+	}
 	LCUIThread_Create( &t, Game_MainThread, NULL );
 	return LCUI_Main();
 }
