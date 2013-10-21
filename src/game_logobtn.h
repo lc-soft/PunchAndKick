efe2aedf70a3c582bb6b397f2b9109c07c4c7e74
Liu Chao <lc-soft@live.cn>
2013-10-21 12:08:54 +0800

    添加LOGO按钮部件

---
+++ src/game_logobtn.h
@@ -0,0 +1,14 @@
+﻿#ifndef __GAME_LOGO_BUTTON__
+#define __GAME_LOGO_BUTTON__
+
+LCUI_Widget* LogoBtn_New(void);
+
+void LogoBtn_SetStyle(	LCUI_Widget *widget, 
+			LCUI_Graph* logo, 
+			LCUI_Graph *bg_normal, 
+			LCUI_Graph *bg_hover,
+			LCUI_Graph *bg_pressed );
+
+void LogoBtn_Register(void);
+
+#endif
