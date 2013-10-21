65c0fb61c1ff54581b864114cb27729187bb34b2
Liu Chao <lc-soft@live.cn>
2013-10-21 12:09:17 +0800

    添加主菜单栏

--- src/main_menu.c
+++ src/main_menu.c
@@ -6,14 +6,31 @@
 #include LC_INPUT_H
 
 #include "game.h"
+#include "game_logobtn.h"
+
 #define ITEM_BOX_HEIGHT	50
+#define LOGOBTN_SIZE Size(116,116)
+
+enum RES_ID {
+	RES_MAIN_BG,
+	RES_FRONT_WAVE,
+	RES_BACK_WAVE,
+	RES_KEY_TIP,
+	RES_TITEBAR_BG,
+	RES_LOGO_NORMAL,
+	RES_LOGOBTN_NORMAL,
+	RES_LOGOBTN_HOVER,
+	RES_LOGOBTN_PRESSED
+};
 
 static LCUI_Widget *main_menu_box;
+static LCUI_Widget *main_menu_titlebar;
+static LCUI_Widget *logobtn;
 static LCUI_Widget *keyboard_tip_box;
 static LCUI_Widget *main_menu_item_box;
 static LCUI_Widget *front_wave[2], *back_wave[2];
 static LCUI_Widget *copyright_text;
-static LCUI_Graph img_res[4];
+static LCUI_Graph img_res[9];
 
 static void UIEffect_MoveWave1( void *arg )
 {
@@ -71,10 +88,15 @@ static void UIEffect_MoveWave2( void *arg )
 
 void Game_LoadMainMenuRes(void)
 {
-	GameGraphRes_GetGraph( MAIN_RES, "main-menu-bg", &img_res[0] );
-	GameGraphRes_GetGraph( MAIN_RES, "front-wave-img", &img_res[1] );
-	GameGraphRes_GetGraph( MAIN_RES, "back-wave-img", &img_res[2] );
-	GameGraphRes_GetGraph( MAIN_RES, "keyboard-control-method", &img_res[3] );
+	GameGraphRes_GetGraph( MAIN_RES, "main-menu-bg", &img_res[RES_MAIN_BG] );
+	GameGraphRes_GetGraph( MAIN_RES, "front-wave-img", &img_res[RES_FRONT_WAVE] );
+	GameGraphRes_GetGraph( MAIN_RES, "back-wave-img", &img_res[RES_BACK_WAVE] );
+	GameGraphRes_GetGraph( MAIN_RES, "keyboard-control-method", &img_res[RES_KEY_TIP] );
+	GameGraphRes_GetGraph( MAIN_RES, "main-menu-titlebar", &img_res[RES_TITEBAR_BG] );
+	GameGraphRes_GetGraph( MAIN_RES, "logo-btn-img", &img_res[RES_LOGO_NORMAL] );
+	GameGraphRes_GetGraph( MAIN_RES, "logo-btn-hover", &img_res[RES_LOGOBTN_HOVER] );
+	GameGraphRes_GetGraph( MAIN_RES, "logo-btn-normal", &img_res[RES_LOGOBTN_NORMAL] );
+	GameGraphRes_GetGraph( MAIN_RES, "logo-btn-pressed", &img_res[RES_LOGOBTN_PRESSED] );
 }
 
 static void keyboard_tip_box_on_drag( LCUI_Widget *widget, LCUI_WidgetEvent *event )
@@ -112,6 +134,8 @@ void Game_InitMainMenu(void)
 	Game_LoadMainMenuRes();
 	/* 创建所需的部件 */
 	main_menu_box = Widget_New(NULL);
+	main_menu_titlebar = Widget_New(NULL);
+	logobtn = LogoBtn_New();
 	main_menu_item_box = Widget_New(NULL);
 	keyboard_tip_box = Widget_New(NULL);
 	front_wave[0] = Widget_New(NULL);
@@ -123,11 +147,23 @@ void Game_InitMainMenu(void)
 	Widget_SetSize( main_menu_box, "100%", "100%" );
 	Widget_SetAlign( main_menu_box, ALIGN_MIDDLE_CENTER, Pos(0,0) );
 	Widget_SetAlpha( main_menu_box, 0 );
-	Widget_SetBackgroundImage( main_menu_box, &img_res[0] );
+	Widget_SetBackgroundImage( main_menu_box, &img_res[RES_MAIN_BG] );
 	Widget_SetBackgroundLayout( main_menu_box, LAYOUT_TILE );
 	Widget_SetBackgroundTransparent( main_menu_box, FALSE );
 	/* 主菜单框显示在普通部件的底层 */
 	Widget_SetZIndex( main_menu_box, -1 );
+	
+	/* 配置主菜单栏 */
+	Widget_Container_Add( main_menu_box, main_menu_titlebar );
+	Widget_SetBackgroundImage( main_menu_titlebar, &img_res[RES_TITEBAR_BG] );
+	Widget_Resize( main_menu_titlebar, Graph_GetSize(&img_res[RES_TITEBAR_BG]) );
+	Widget_SetAlign( main_menu_titlebar, ALIGN_TOP_LEFT, Pos(0,30) );
+	/* 配置LOGO按钮 */
+	Widget_Container_Add( main_menu_titlebar, logobtn );
+	Widget_Resize( logobtn, LOGOBTN_SIZE );
+	Widget_SetAlign( logobtn, ALIGN_MIDDLE_LEFT, Pos(10,0) );
+	LogoBtn_SetStyle( logobtn, &img_res[RES_LOGO_NORMAL], &img_res[RES_LOGOBTN_NORMAL],
+				&img_res[RES_LOGOBTN_HOVER], &img_res[RES_LOGOBTN_PRESSED] );
 
 	Widget_Container_Add( main_menu_box, main_menu_item_box );
 	Widget_SetHeight( main_menu_item_box, ITEM_BOX_HEIGHT );
@@ -138,32 +174,32 @@ void Game_InitMainMenu(void)
 
 	Widget_Container_Add( main_menu_box, front_wave[0] );
 	Widget_Container_Add( main_menu_box, front_wave[1] );
-	Widget_Resize( front_wave[0], Graph_GetSize(&img_res[1]) );
-	Widget_Resize( front_wave[1], Graph_GetSize(&img_res[1]) );
-	Widget_SetBackgroundImage( front_wave[0], &img_res[1] );
-	Widget_SetBackgroundImage( front_wave[1], &img_res[1] );
+	Widget_Resize( front_wave[0], Graph_GetSize(&img_res[RES_FRONT_WAVE]) );
+	Widget_Resize( front_wave[1], Graph_GetSize(&img_res[RES_FRONT_WAVE]) );
+	Widget_SetBackgroundImage( front_wave[0], &img_res[RES_FRONT_WAVE] );
+	Widget_SetBackgroundImage( front_wave[1], &img_res[RES_FRONT_WAVE] );
 	Widget_SetBackgroundLayout( front_wave[0], LAYOUT_CENTER );
 	Widget_SetBackgroundLayout( front_wave[1], LAYOUT_CENTER );
 	Widget_SetAlign( front_wave[0], ALIGN_BOTTOM_LEFT, Pos(0,-ITEM_BOX_HEIGHT) );
-	Widget_SetAlign( front_wave[1], ALIGN_BOTTOM_LEFT, Pos(Graph_GetSize(&img_res[1]).w,-ITEM_BOX_HEIGHT) );
+	Widget_SetAlign( front_wave[1], ALIGN_BOTTOM_LEFT, Pos(Graph_GetSize(&img_res[RES_FRONT_WAVE]).w,-ITEM_BOX_HEIGHT) );
 	
 	Widget_Container_Add( main_menu_box, back_wave[0] );
 	Widget_Container_Add( main_menu_box, back_wave[1] );
-	Widget_Resize( back_wave[0], Graph_GetSize(&img_res[2]) );
-	Widget_Resize( back_wave[1], Graph_GetSize(&img_res[2]) );
-	Widget_SetBackgroundImage( back_wave[0], &img_res[2] );
-	Widget_SetBackgroundImage( back_wave[1], &img_res[2] );
+	Widget_Resize( back_wave[0], Graph_GetSize(&img_res[RES_BACK_WAVE]) );
+	Widget_Resize( back_wave[1], Graph_GetSize(&img_res[RES_BACK_WAVE]) );
+	Widget_SetBackgroundImage( back_wave[0], &img_res[RES_BACK_WAVE] );
+	Widget_SetBackgroundImage( back_wave[1], &img_res[RES_BACK_WAVE] );
 	Widget_SetBackgroundLayout( back_wave[0], LAYOUT_CENTER );
 	Widget_SetBackgroundLayout( back_wave[1], LAYOUT_CENTER );
 	Widget_SetAlign( back_wave[0], ALIGN_BOTTOM_LEFT, Pos(0,-ITEM_BOX_HEIGHT) );
-	Widget_SetAlign( back_wave[1], ALIGN_BOTTOM_LEFT, Pos(Graph_GetSize(&img_res[2]).w,-ITEM_BOX_HEIGHT) );
+	Widget_SetAlign( back_wave[1], ALIGN_BOTTOM_LEFT, Pos(Graph_GetSize(&img_res[RES_BACK_WAVE]).w,-ITEM_BOX_HEIGHT) );
 	
 	Widget_Container_Add( main_menu_box, copyright_text );
 	Label_TextW( copyright_text, L"<size=16px>Powered By LCUI</size>" );
 	Widget_SetAlign( copyright_text, ALIGN_BOTTOM_CENTER, Pos(0,-30) );
 	
-	Widget_SetBackgroundImage( keyboard_tip_box, &img_res[3] );
-	Widget_Resize( keyboard_tip_box, Graph_GetSize(&img_res[3]) );
+	Widget_SetBackgroundImage( keyboard_tip_box, &img_res[RES_KEY_TIP] );
+	Widget_Resize( keyboard_tip_box, Graph_GetSize(&img_res[RES_KEY_TIP]) );
 	Widget_SetAlign( keyboard_tip_box, ALIGN_MIDDLE_CENTER, Pos(0,0) );
 	Widget_Event_Connect( keyboard_tip_box, EVENT_DRAG, keyboard_tip_box_on_drag );
 	Widget_Event_Connect( keyboard_tip_box, EVENT_CLICKED, keyboard_tip_box_on_clicked );
@@ -172,6 +208,8 @@ void Game_InitMainMenu(void)
 	Widget_Show( front_wave[1] );
 	Widget_Show( back_wave[0] );
 	Widget_Show( back_wave[1] );
+	Widget_Show( logobtn );
+	Widget_Show( main_menu_titlebar );
 	Widget_Show( main_menu_item_box );
 	Widget_Show( copyright_text );
 	Widget_Show( main_menu_box );
@@ -193,7 +231,7 @@ void Game_ShowMainMenu(void)
 	}
 	Widget_SetAlpha( main_menu_box, 255 );
 	/* 显示按键提示框 */
-	Widget_Show( keyboard_tip_box );
+	//Widget_Show( keyboard_tip_box );
 }
 
 /** 隐藏主菜单 */
@@ -201,7 +239,7 @@ void Game_HideMainMenu(void)
 {
 	uchar_t alpha;
 	/* 隐藏按键提示框 */
-	Widget_Hide( keyboard_tip_box );
+	//Widget_Hide( keyboard_tip_box );
 	/* 以淡出的效果显示菜单 */
 	for( alpha=240; alpha>0; alpha-=20 ) {
 		Widget_SetAlpha( main_menu_box, alpha );
