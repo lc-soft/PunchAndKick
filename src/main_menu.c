60b6d96e9ae91710beaa4094fa2994ec388c4aef
Liu Chao <lc-soft@live.cn>
2013-10-23 20:12:33 +0800

    添加帮助菜单

--- src/main_menu.c
+++ src/main_menu.c
@@ -20,6 +20,13 @@
 #define TEXT_HELP		L"帮助"
 #define TEXT_QUIT		L"退出"
 
+#define TEXT_USAGE		L"操作方法"
+#define TEXT_ABOUT		L"关于本游戏"
+#define TEXT_LICENSE		L"许可协议"
+
+#define SIZE_MAIN_MENU_BOX	Size(150,174)
+#define SIZE_HELP_MENU_BOX	Size(150,110)
+
 #define COLOR_RED	RGB(255,78,0)
 #define COLOR_ORANGE	RGB(255,197,2)
 #define COLOR_GREEN	RGB(76,133,2)
@@ -38,12 +45,11 @@ enum RES_ID {
 	RES_LOGOBTN_PRESSED
 };
 
-static LCUI_Widget *main_menu_box;
-static LCUI_Widget *main_menu_titlebar;
+static LCUI_Widget *main_ui_box;
+static LCUI_Widget *main_menu_box, *help_menu_box;
 static LCUI_Widget *btn_single, *btn_battle, *btn_options, *btn_help, *btn_quit;
-static LCUI_Widget *logobtn;
-static LCUI_Widget *keyboard_tip_box;
-static LCUI_Widget *main_menu_item_box;
+static LCUI_Widget *btn_usage, *btn_about, *btn_license;
+static LCUI_Widget *footer_box;
 static LCUI_Widget *front_wave[2], *back_wave[2];
 static LCUI_Widget *copyright_text;
 static LCUI_Graph img_res[9];
@@ -150,76 +156,76 @@ static void btn_single_clicked( LCUI_Widget *widget, LCUI_WidgetEvent *unused )
 	Game_ShowRoleSelectBox();	/* 显示角色选择框 */
 }
 
-static void Game_InitMenuBar(void)
+/** 初始化帮助菜单按钮 */
+static void Game_InitHelpMenuButton(void)
 {
-	logobtn = LogoBtn_New();
-	btn_single = TitleBarBtn_New();
-	btn_help = TitleBarBtn_New();
-	btn_battle = TitleBarBtn_New();
-	btn_options = TitleBarBtn_New();
-	main_menu_titlebar = Widget_New(NULL);
-	/* 配置主菜单栏 */
-	Widget_Container_Add( main_menu_box, main_menu_titlebar );
-	Widget_SetBackgroundImage( main_menu_titlebar, &img_res[RES_TITEBAR_BG] );
-	Widget_Resize( main_menu_titlebar, Graph_GetSize(&img_res[RES_TITEBAR_BG]) );
-	Widget_SetAlign( main_menu_titlebar, ALIGN_TOP_LEFT, Pos(0,30) );
-	/* 配置LOGO按钮 */
-	Widget_Container_Add( main_menu_titlebar, logobtn );
-	Widget_Resize( logobtn, LOGOBTN_SIZE );
-	Widget_SetAlign( logobtn, ALIGN_MIDDLE_LEFT, Pos(10,0) );
-	LogoBtn_SetStyle( logobtn, &img_res[RES_LOGO_NORMAL], &img_res[RES_LOGOBTN_NORMAL],
-				&img_res[RES_LOGOBTN_HOVER], &img_res[RES_LOGOBTN_PRESSED] );
-	/* 配置菜单栏上的按钮 */
-	Widget_Container_Add( main_menu_titlebar, btn_single );
-	Widget_Container_Add( main_menu_titlebar, btn_battle );
-	Widget_Container_Add( main_menu_titlebar, btn_options );
-	Widget_Container_Add( main_menu_titlebar, btn_help );
+	help_menu_box = Widget_New(NULL);
+	btn_usage = GameMenuBtn_New();
+	btn_about = GameMenuBtn_New();
+	btn_license = GameMenuBtn_New();
 	
-	Widget_SetAlign( btn_single, ALIGN_MIDDLE_LEFT, Pos(130,-2) );
-	Widget_SetAlign( btn_battle, ALIGN_MIDDLE_LEFT, Pos(130+100,-2) );
-	Widget_SetAlign( btn_options, ALIGN_MIDDLE_LEFT, Pos(130+200,-2) );
-	Widget_SetAlign( btn_help, ALIGN_MIDDLE_LEFT, Pos(130+300,-2) );
+	Widget_Container_Add( main_ui_box, help_menu_box );
+	Widget_Container_Add( help_menu_box, btn_usage );
+	Widget_Container_Add( help_menu_box, btn_about );
+	Widget_Container_Add( help_menu_box, btn_license );
 	
-	TitleBarBtn_TextW( btn_single, TEXT_SINGLE_GAME );
-	TitleBarBtn_TextW( btn_battle, TEXT_NETWORK_BATTLE );
-	TitleBarBtn_TextW( btn_options, TEXT_OPTIONS );
-	TitleBarBtn_TextW( btn_help, TEXT_HELP );
+	Widget_SetAlign( help_menu_box, ALIGN_MIDDLE_LEFT, Pos(200,0) );
+	Widget_SetAlign( btn_usage, ALIGN_MIDDLE_LEFT, Pos(0,-35) );
+	Widget_SetAlign( btn_about, ALIGN_MIDDLE_LEFT, Pos(0,0) );
+	Widget_SetAlign( btn_license, ALIGN_MIDDLE_LEFT, Pos(0,35) );
 	
-	Widget_Resize( btn_single, Size(100,65) );
-	Widget_Resize( btn_battle, Size(100,65) );
-	Widget_Resize( btn_options, Size(100,65) );
-	Widget_Resize( btn_help, Size(100,65) );
+	Widget_Resize( help_menu_box, SIZE_HELP_MENU_BOX );
+	Widget_Resize( btn_usage, Size(150,34) );
+	Widget_Resize( btn_about, Size(150,34) );
+	Widget_Resize( btn_license, Size(150,34) );
 
-	Widget_Event_Connect( btn_single, EVENT_CLICKED, btn_single_clicked );
+	GameMenuBtn_SetTextW( btn_usage, TEXT_USAGE );
+	GameMenuBtn_SetTextW( btn_about, TEXT_ABOUT );
+	GameMenuBtn_SetTextW( btn_license, TEXT_LICENSE );
 
-	Widget_Show( logobtn );
-	Widget_Show( btn_single );
-	Widget_Show( btn_battle );
-	Widget_Show( btn_options );
-	Widget_Show( btn_help );
-	Widget_Show( main_menu_titlebar );
+	GameMenuBtn_SetFontSize( btn_usage, 16 );
+	GameMenuBtn_SetFontSize( btn_about, 16 );
+	GameMenuBtn_SetFontSize( btn_license, 16 );
+
+	GameMenuBtn_SetForeWidgetColor( btn_usage, COLOR_RED );
+	GameMenuBtn_SetForeWidgetColor( btn_about, COLOR_ORANGE );
+	GameMenuBtn_SetForeWidgetColor( btn_license, COLOR_GREEN );
+	
+	Widget_Show( btn_usage );
+	Widget_Show( btn_about );
+	Widget_Show( btn_license );
 }
 
+static void Game_ShowHelpMenuButton(void)
+{
+	Widget_Show( help_menu_box );
+}
+
+/** 初始化主菜单按钮 */
 void Game_InitMenuButton(void)
 {
+	main_menu_box = Widget_New(NULL);
 	btn_single = GameMenuBtn_New();
 	btn_battle = GameMenuBtn_New();
 	btn_options = GameMenuBtn_New();
 	btn_help = GameMenuBtn_New();
 	btn_quit = GameMenuBtn_New();
-
+	
+	Widget_Container_Add( main_ui_box, main_menu_box );
 	Widget_Container_Add( main_menu_box, btn_single );
 	Widget_Container_Add( main_menu_box, btn_battle );
 	Widget_Container_Add( main_menu_box, btn_options );
 	Widget_Container_Add( main_menu_box, btn_help );
 	Widget_Container_Add( main_menu_box, btn_quit );
+	
+	Widget_SetAlign( main_menu_box, ALIGN_MIDDLE_LEFT, Pos(50,0) );
+	Widget_SetAlign( btn_single, ALIGN_MIDDLE_LEFT, Pos(0,-70) );
+	Widget_SetAlign( btn_battle, ALIGN_MIDDLE_LEFT, Pos(0,-35) );
+	Widget_SetAlign( btn_options, ALIGN_MIDDLE_LEFT, Pos(0,0) );
+	Widget_SetAlign( btn_help, ALIGN_MIDDLE_LEFT, Pos(0,35) );
+	Widget_SetAlign( btn_quit, ALIGN_MIDDLE_LEFT, Pos(0,70) );
 
-	Widget_SetAlign( btn_single, ALIGN_MIDDLE_LEFT, Pos(50,-70) );
-	Widget_SetAlign( btn_battle, ALIGN_MIDDLE_LEFT, Pos(50,-35) );
-	Widget_SetAlign( btn_options, ALIGN_MIDDLE_LEFT, Pos(50,0) );
-	Widget_SetAlign( btn_help, ALIGN_MIDDLE_LEFT, Pos(50,35) );
-	Widget_SetAlign( btn_quit, ALIGN_MIDDLE_LEFT, Pos(50,70) );
-
+	Widget_Resize( main_menu_box, SIZE_MAIN_MENU_BOX );
 	Widget_Resize( btn_single, Size(150,34) );
 	Widget_Resize( btn_battle, Size(150,34) );
 	Widget_Resize( btn_options, Size(150,34) );
@@ -251,37 +257,42 @@ void Game_InitMenuButton(void)
 	Widget_Show( btn_quit );
 }
 
+static void Game_ShowMenuButton(void)
+{
+	Widget_Show( main_menu_box );
+}
+
 /** 初始化主菜单 */
 void Game_InitMainMenu(void)
 {
 	Game_LoadMainMenuRes();
 	/* 创建所需的部件 */
-	main_menu_box = Widget_New(NULL);
+	main_ui_box = Widget_New(NULL);
 
-	main_menu_item_box = Widget_New(NULL);
-	keyboard_tip_box = Widget_New(NULL);
+	footer_box = Widget_New(NULL);
 	front_wave[0] = Widget_New(NULL);
 	front_wave[1] = Widget_New(NULL);
 	back_wave[0] = Widget_New(NULL);
 	back_wave[1] = Widget_New(NULL);
 	copyright_text = Widget_New("label");
 
-	Widget_SetSize( main_menu_box, "100%", "100%" );
-	Widget_SetAlign( main_menu_box, ALIGN_MIDDLE_CENTER, Pos(0,0) );
-	Widget_SetAlpha( main_menu_box, 0 );
-	Widget_SetBackgroundColor( main_menu_box, RGB(240,240,240) );
-	Widget_SetBackgroundTransparent( main_menu_box, FALSE );
-	/* 主菜单框显示在普通部件的底层 */
-	Widget_SetZIndex( main_menu_box, -1 );
-	Widget_Container_Add( main_menu_box, main_menu_item_box );
-	Widget_SetHeight( main_menu_item_box, ITEM_BOX_HEIGHT );
-	Widget_SetSize( main_menu_item_box, "100%", NULL );
-	Widget_SetAlign( main_menu_item_box, ALIGN_BOTTOM_CENTER, Pos(0,0) );
-	Widget_SetBackgroundColor( main_menu_item_box, RGB(255,255,255) );
-	Widget_SetBackgroundTransparent ( main_menu_item_box, FALSE );
+	Widget_SetSize( main_ui_box, "100%", "100%" );
+	Widget_SetAlign( main_ui_box, ALIGN_MIDDLE_CENTER, Pos(0,0) );
+	Widget_SetAlpha( main_ui_box, 0 );
+	Widget_SetBackgroundColor( main_ui_box, RGB(240,240,240) );
+	Widget_SetBackgroundTransparent( main_ui_box, FALSE );
+	
+	/* 主界面显示在普通部件的底层 */
+	Widget_SetZIndex( main_ui_box, -1 );
+	Widget_Container_Add( main_ui_box, footer_box );
+	Widget_SetHeight( footer_box, ITEM_BOX_HEIGHT );
+	Widget_SetSize( footer_box, "100%", NULL );
+	Widget_SetAlign( footer_box, ALIGN_BOTTOM_CENTER, Pos(0,0) );
+	Widget_SetBackgroundColor( footer_box, RGB(255,255,255) );
+	Widget_SetBackgroundTransparent ( footer_box, FALSE );
 
-	Widget_Container_Add( main_menu_box, front_wave[0] );
-	Widget_Container_Add( main_menu_box, front_wave[1] );
+	Widget_Container_Add( main_ui_box, front_wave[0] );
+	Widget_Container_Add( main_ui_box, front_wave[1] );
 	Widget_Resize( front_wave[0], Graph_GetSize(&img_res[RES_FRONT_WAVE]) );
 	Widget_Resize( front_wave[1], Graph_GetSize(&img_res[RES_FRONT_WAVE]) );
 	Widget_SetBackgroundImage( front_wave[0], &img_res[RES_FRONT_WAVE] );
@@ -291,8 +302,8 @@ void Game_InitMainMenu(void)
 	Widget_SetAlign( front_wave[0], ALIGN_BOTTOM_LEFT, Pos(0,-ITEM_BOX_HEIGHT) );
 	Widget_SetAlign( front_wave[1], ALIGN_BOTTOM_LEFT, Pos(Graph_GetSize(&img_res[RES_FRONT_WAVE]).w,-ITEM_BOX_HEIGHT) );
 	
-	Widget_Container_Add( main_menu_box, back_wave[0] );
-	Widget_Container_Add( main_menu_box, back_wave[1] );
+	Widget_Container_Add( main_ui_box, back_wave[0] );
+	Widget_Container_Add( main_ui_box, back_wave[1] );
 	Widget_Resize( back_wave[0], Graph_GetSize(&img_res[RES_BACK_WAVE]) );
 	Widget_Resize( back_wave[1], Graph_GetSize(&img_res[RES_BACK_WAVE]) );
 	Widget_SetBackgroundImage( back_wave[0], &img_res[RES_BACK_WAVE] );
@@ -302,26 +313,22 @@ void Game_InitMainMenu(void)
 	Widget_SetAlign( back_wave[0], ALIGN_BOTTOM_LEFT, Pos(0,-ITEM_BOX_HEIGHT) );
 	Widget_SetAlign( back_wave[1], ALIGN_BOTTOM_LEFT, Pos(Graph_GetSize(&img_res[RES_BACK_WAVE]).w,-ITEM_BOX_HEIGHT) );
 	
-	Widget_Container_Add( main_menu_box, copyright_text );
+	Widget_Container_Add( main_ui_box, copyright_text );
 	Label_TextW( copyright_text, L"<size=16px>Powered By LCUI</size>" );
 	Widget_SetAlign( copyright_text, ALIGN_BOTTOM_CENTER, Pos(0,-30) );
 	
-	Widget_SetBackgroundImage( keyboard_tip_box, &img_res[RES_KEY_TIP] );
-	Widget_Resize( keyboard_tip_box, Graph_GetSize(&img_res[RES_KEY_TIP]) );
-	Widget_SetAlign( keyboard_tip_box, ALIGN_MIDDLE_CENTER, Pos(0,0) );
-	Widget_Event_Connect( keyboard_tip_box, EVENT_DRAG, keyboard_tip_box_on_drag );
-	Widget_Event_Connect( keyboard_tip_box, EVENT_CLICKED, keyboard_tip_box_on_clicked );
-	
-	//Game_InitMenuBar();
 	Game_InitMenuButton();
+	Game_InitHelpMenuButton();
+	Game_ShowMenuButton();
+	Game_ShowHelpMenuButton();
 
 	Widget_Show( front_wave[0] );
 	Widget_Show( front_wave[1] );
 	Widget_Show( back_wave[0] );
 	Widget_Show( back_wave[1] );
-	Widget_Show( main_menu_item_box );
+	Widget_Show( footer_box );
 	Widget_Show( copyright_text );
-	Widget_Show( main_menu_box );
+	Widget_Show( main_ui_box );
 	/* 设置两个定时器，用于定时改变波浪的位置，以实现波浪的移动效果 */
 	LCUITimer_Set( 20, UIEffect_MoveWave1, NULL, TRUE );
 	LCUITimer_Set( 60, UIEffect_MoveWave2, NULL, TRUE );
@@ -332,33 +339,31 @@ void Game_ShowMainMenu(void)
 {
 	uchar_t alpha;
 
-	Widget_Show( main_menu_box );
+	Widget_Show( main_ui_box );
 	/* 以淡入的效果显示菜单 */
 	for( alpha=0; alpha<240; alpha+=20 ) {
-		Widget_SetAlpha( main_menu_box, alpha );
+		Widget_SetAlpha( main_ui_box, alpha );
 		LCUI_MSleep(25);
 	}
-	Widget_SetAlpha( main_menu_box, 255 );
+	Widget_SetAlpha( main_ui_box, 255 );
 }
 
 LCUI_Widget* Game_GetMainMenuBox(void)
 {
-	return main_menu_box;
+	return main_ui_box;
 }
 
 /** 隐藏主菜单 */
 void Game_HideMainMenu(void)
 {
 	uchar_t alpha;
-	/* 隐藏按键提示框 */
-	Widget_Hide( keyboard_tip_box );
 	/* 以淡出的效果显示菜单 */
 	for( alpha=240; alpha>0; alpha-=20 ) {
-		Widget_SetAlpha( main_menu_box, alpha );
+		Widget_SetAlpha( main_ui_box, alpha );
 		LCUI_MSleep(25);
 	}
-	Widget_SetAlpha( main_menu_box, 0 );
-	Widget_Hide( main_menu_box );
+	Widget_SetAlpha( main_ui_box, 0 );
+	Widget_Hide( main_ui_box );
 }
 
 /** 销毁主菜单 */
