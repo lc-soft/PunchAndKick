4129c30be4cdd1e1b768f1c968fc8928c732e387
Liu Chao <lc-soft@live.cn>
2013-10-26 16:58:16 +0800

    添加”许可协议“窗口，待完善

---
+++ src/gamewnd_license.c
@@ -0,0 +1,38 @@
+﻿#include <LCUI_Build.h>
+#include LC_LCUI_H
+#include LC_WIDGET_H
+#include LC_WINDOW_H
+
+#define WINDOW_SIZE		Size(400,400)
+#define TEXT_WINDOW_TILE	L"许可协议"
+
+static LCUI_Widget *window;
+static LCUI_Widget *license_textbox;
+
+void GameWindow_InitLicenseWindow(void)
+{
+	window = Widget_New("window");
+	license_textbox = Widget_New("text_box");
+
+	Window_ClientArea_Add( window, license_textbox );
+	Widget_Resize( window, WINDOW_SIZE );
+	Window_SetTitleTextW( window, TEXT_WINDOW_TILE );
+	Widget_SetDock( license_textbox, DOCK_TYPE_FILL );
+	Widget_Show( license_textbox );
+}
+
+void GameWindow_ShowLicenseWindow(void)
+{
+	Widget_SetModal( window, TRUE );
+	Widget_Show( window );
+}
+
+void GameWindow_HideLicenseWindow(void)
+{
+
+}
+
+void GameWindow_DestroyLicenseWindow(void)
+{
+
+}
---
+++ src/gamewnd_license.h
@@ -0,0 +1,12 @@
+#ifndef __GAME_WINDOW_LICENSE_H__
+#define __GAME_WINDOW_LICENSE_H__
+
+void GameWindow_InitLicenseWindow(void);
+
+void GameWindow_ShowLicenseWindow(void);
+
+void GameWindow_HideLicenseWindow(void);
+
+void GameWindow_DestroyLicenseWindow(void);
+
+#endif
