5289c0450427f3bccf9127ef32fa16b6a1f955bc
Liu Chao <lc-soft@live.cn>
2013-10-30 13:06:44 +0800

    基础game_scene.h的包含

--- src/game.h
+++ src/game.h
@@ -11,7 +11,6 @@ LCUI_BEGIN_HEADER
 #include "game_resource.h"
 #include "game_action.h"
 #include "game_space.h"
-#include "game_scene.h"
 #include "game_msg.h"
 #include "game_statusbar.h"
 #include "game_attack.h"
