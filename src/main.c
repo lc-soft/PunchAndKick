a7fcf5273ee29da07d515408cd7f98de44a12191
Liu Chao <lc-soft@live.cn>
2013-10-30 13:08:01 +0800

    添加单人游戏对战

--- src/main.c
+++ src/main.c
@@ -33,6 +33,8 @@
 
 
 
+static LCUI_Widget *player_status_area;
+
 /** 在屏幕上显示游戏启动时的画面 */
 static void Game_ShowBootScreen(void)
 {
@@ -117,24 +119,18 @@ static void Game_ShowBootScreen(void)
 	Widget_Destroy( wdg_copyright_cn_text );
 }
 
-/** 加入CPU玩家 */
-static void Game_AddCPUPlayer(void)
+/** 加入CPU玩家角色 */
+static void Game_AddCPUPlayerRole( int battle_id, int role_id[4] )
 {
-#ifdef aaaa
-	int i, j, role_id;
-	GamePlayer *player, *other_player;
+	int i, j;
+
 	for(j=2; j<5; ++j) {
-		player = GamePlayer_GetByID(j);
 		while(1) {
 			/* 随机选择一个角色 */
-			role_id = rand()%TOTAL_ROLE_NUM;
+			role_id[j-1] = rand()%TOTAL_ROLE_NUM;
 			/* 检查是否与前面几个角色的ID相同 */
 			for(i=1; i<j; ++i) {
-				other_player = GamePlayer_GetByID(i);
-				if( !other_player ) {
-					continue;	
-				}
-				if( role_id == other_player->role_id ) {
+				if( role_id[j-1] == role_id[i-1] ) {
 					break;
 				}
 			}
@@ -142,15 +138,11 @@ static void Game_AddCPUPlayer(void)
 				break;
 			}
 		}
-		Game_EnableGamePlayer( j );
-		Game_SetGamePlayer( j, role_id, FALSE );
 	}
-#endif
 }
 
 static void GameDemoScene_PutPlayer( int battle_id )
 {
-	int i;
 	int x, y;
 	LCUI_Pos start_pos;
 	LCUI_Size scene_size;
@@ -202,19 +194,257 @@ static void Game_DemoThread( void *arg )
 	GameBattle_Loop( battle_id );
 }
 
+/** 初始化游戏角色的位置 */
+static void Game_InitPlayerPos( int battle_id )
+{
+	int x, y;
+	GamePlayer *p_player[4];
+	LCUI_Pos land_pos;
+	LCUI_Size land_size;
+
+	p_player[0] = GameBattle_GetPlayer( battle_id, 1 );
+	p_player[1] = GameBattle_GetPlayer( battle_id, 2 );
+	p_player[2] = GameBattle_GetPlayer( battle_id, 3 );
+	p_player[3] = GameBattle_GetPlayer( battle_id, 4 );
+
+	GameBattle_GetSceneLand( battle_id, &land_pos, &land_size );
+	/* 计算并设置游戏角色的位置 */
+	x = land_size.w/2 - 150;
+	GameObject_SetX( p_player[0]->object, land_pos.x+x );
+	GameObject_SetX( p_player[1]->object, land_pos.x+x-50 );
+	x = land_size.w/2 + 150;
+	GameObject_SetX( p_player[2]->object, land_pos.x+x );
+	GameObject_SetX( p_player[3]->object, land_pos.x+x+50 );
+	y = land_size.h/2;
+	GameObject_SetY( p_player[0]->object, land_pos.y+y-50 );
+	GameObject_SetY( p_player[1]->object, land_pos.y+y+50 );
+	GameObject_SetY( p_player[2]->object, land_pos.y+y-50 );
+	GameObject_SetY( p_player[3]->object, land_pos.y+y+50 );
+	/* 改变游戏角色的朝向 */
+	GamePlayer_SetRightOriented( p_player[0] );
+	GamePlayer_SetRightOriented( p_player[1] );
+	GamePlayer_SetLeftOriented( p_player[2] );
+	GamePlayer_SetLeftOriented( p_player[3] );
+}
+
+static void Game_UpdatePlayerStatusBar( int battle_id )
+{
+	int i;
+	RoleInfo *p_role_info;
+	wchar_t player_type_name[5];
+	GamePlayer *p_player[4];
+
+	p_player[0] = GameBattle_GetPlayer( battle_id, 1 );
+	p_player[1] = GameBattle_GetPlayer( battle_id, 2 );
+	p_player[2] = GameBattle_GetPlayer( battle_id, 3 );
+	p_player[3] = GameBattle_GetPlayer( battle_id, 4 );
+
+	for(i=0; i<4; ++i) {
+		if( p_player[i]->human_control ) {
+			player_type_name[0] = L'1'+i;
+			player_type_name[1] = L'P';
+			player_type_name[2] = 0;
+		} else {
+			player_type_name[0] = L'C';
+			player_type_name[1] = L'P';
+			player_type_name[2] = L'U';
+			player_type_name[3] = 0;
+		}
+		p_role_info = Game_GetRoleInfo( p_player[i]->role_id );
+		StatusBar_SetPlayerNameW( p_player[i]->statusbar, p_role_info->name );
+		/* 设置角色类型名 */
+		StatusBar_SetPlayerTypeNameW( p_player[i]->statusbar, player_type_name );
+		/* 设置血量 */
+		StatusBar_SetHealth( p_player[i]->statusbar, p_player[i]->property.cur_hp );
+		StatusBar_SetMaxHealth( p_player[i]->statusbar, p_player[i]->property.max_hp );
+		/* 显示状态栏 */
+		Widget_Show( p_player[i]->statusbar );
+	}
+}
+
+static void UpdateViewFPS( void *arg )
+{
+	char str[10];
+	LCUI_Widget *label = (LCUI_Widget*)arg;
+	sprintf( str, "FPS: %d", LCUIScreen_GetFPS() );
+	Label_Text( label, str );
+	_DEBUG_MSG("tip, %s\n", str);
+}
+
+static void InitSceneText( LCUI_Widget *scene )
+{
+	LCUI_Widget *text, *fps_text;
+	LCUI_TextStyle style;
+
+	text = Widget_New("label");
+	fps_text = Widget_New("label");
+	Widget_Container_Add( scene, text );
+	Widget_Container_Add( scene, fps_text );
+	TextStyle_Init( &style );
+	TextStyle_FontSize( &style, 18 );
+	Label_TextStyle( fps_text, style );
+	Widget_SetAlign( text, ALIGN_TOP_CENTER, Pos(0,40) );
+	Widget_SetAlign( fps_text, ALIGN_TOP_CENTER, Pos(0,100) );
+	Label_TextW( text, L"<size=42px>游戏测试</size>");
+	Widget_SetZIndex( fps_text, -5000 );
+	Widget_SetZIndex( text, -5000 );
+	Widget_Show( fps_text );
+	Widget_Show( text );
+	_DEBUG_MSG("tip\n");
+	LCUITimer_Set( 500, UpdateViewFPS, fps_text, TRUE );
+}
+
+static void Game_ShowPlayerStatusArea(void)
+{
+	Widget_Show( player_status_area );
+}
+
+static int Game_DestroyPlayerStatusArea( int battle_id )
+{
+
+}
+
+static int Game_InitPlayerStatusArea( int battle_id )
+{
+	GamePlayer *p_player[4];
+
+	p_player[0] = GameBattle_GetPlayer( battle_id, 1 );
+	p_player[1] = GameBattle_GetPlayer( battle_id, 2 );
+	p_player[2] = GameBattle_GetPlayer( battle_id, 3 );
+	p_player[3] = GameBattle_GetPlayer( battle_id, 4 );
+
+	player_status_area = Widget_New(NULL);
+	Widget_SetBackgroundColor( player_status_area, RGB(240,240,240) );
+	Widget_SetBackgroundTransparent( player_status_area, FALSE );
+	Widget_SetBorder( player_status_area, Border(1,BORDER_STYLE_SOLID,RGB(150,150,150)));
+	Widget_Resize( player_status_area, Size(800,STATUS_BAR_HEIGHT) );
+	Widget_SetAlign( player_status_area, ALIGN_BOTTOM_CENTER, Pos(0,0) );
+	/* 创建状态栏 */
+	p_player[0]->statusbar = StatusBar_New();
+	p_player[1]->statusbar = StatusBar_New();
+	p_player[2]->statusbar = StatusBar_New();
+	p_player[3]->statusbar = StatusBar_New();
+
+	Widget_Container_Add( player_status_area, p_player[0]->statusbar );
+	Widget_Container_Add( player_status_area, p_player[1]->statusbar );
+	Widget_Container_Add( player_status_area, p_player[2]->statusbar );
+	Widget_Container_Add( player_status_area, p_player[3]->statusbar );
+	Widget_SetAlign( p_player[0]->statusbar, ALIGN_TOP_LEFT, Pos(5,5) );
+	Widget_SetAlign( p_player[1]->statusbar, ALIGN_TOP_LEFT, Pos(5+200,5) );
+	Widget_SetAlign( p_player[2]->statusbar, ALIGN_TOP_LEFT, Pos(5+400,5) );
+	Widget_SetAlign( p_player[3]->statusbar, ALIGN_TOP_LEFT, Pos(5+600,5) );
+	return 0;
+}
+
+static int Game_InitFight( int role_id[4] )
+{
+	int ret, battle_id;
+	LCUI_Graph stage_img;
+	LCUI_Pos land_pos;
+	ControlKey ctrlkey;
+	LCUI_Size land_size, scene_size;
+	
+	ret = GameGraphRes_LoadFromFile("scenes.data");
+	if( ret != 0 ) {
+		LCUI_MessageBoxW(
+			MB_ICON_ERROR,
+			L"场景资源载入出错，请检查程序的完整性！",
+			L"错误", MB_BTN_OK );
+		return ret;
+	}
+	
+	Graph_Init( &stage_img );
+	/* 载入战斗场景的图形资源 */
+	GameGraphRes_GetGraph( SCENES_RES, "default", &stage_img );
+
+	battle_id = GameBattle_New();
+	scene_size = Graph_GetSize( &stage_img );
+	land_pos.x = 20;
+	land_pos.y = 268;
+	land_size.w = scene_size.w - 2*land_pos.x;
+	land_size.h = scene_size.h - land_pos.y;
+
+	GameBattle_SetSceneBackground( battle_id, &stage_img );
+	GameBattle_SetSceneSize( battle_id, scene_size );
+	GameBattle_SetSceneLand( battle_id, land_pos, land_size );
+
+	Game_AddCPUPlayerRole( battle_id, role_id );
+	GameBattle_SetPlayer( battle_id, 1, role_id[0], TRUE );
+	GameBattle_SetPlayer( battle_id, 2, role_id[1], FALSE );
+	GameBattle_SetPlayer( battle_id, 3, role_id[2], FALSE );
+	GameBattle_SetPlayer( battle_id, 4, role_id[3], FALSE );
+	GameBattle_EnablePlayer( battle_id, 1, TRUE );
+	GameBattle_EnablePlayer( battle_id, 2, TRUE );
+	GameBattle_EnablePlayer( battle_id, 3, TRUE );
+	GameBattle_EnablePlayer( battle_id, 4, TRUE );
+
+	/* 设置战斗场景显示的位置 */
+	Widget_SetAlign( GameBattle_GetScene(battle_id), 
+	ALIGN_BOTTOM_CENTER, Pos(0,-STATUS_BAR_HEIGHT) );
+	/* 初始化在场景上显示的文本 */
+	InitSceneText( GameBattle_GetScene(battle_id) );
+	
+	ctrlkey.a_attack = LCUIKEY_J;
+	ctrlkey.b_attack = LCUIKEY_K;
+	ctrlkey.jump = LCUIKEY_SPACE;
+	ctrlkey.defense = LCUIKEY_L;
+	ctrlkey.left = LCUIKEY_A;
+	ctrlkey.right = LCUIKEY_D;
+	ctrlkey.up = LCUIKEY_W;
+	ctrlkey.down = LCUIKEY_S;
+	/* 设置1号玩家的控制键 */
+	GameBattle_SetPlayerControlKey( battle_id, 1, &ctrlkey );
+
+	Game_InitPlayerStatusArea( battle_id );
+	Game_InitPlayerPos( battle_id );
+	Game_UpdatePlayerStatusBar( battle_id );
+	GameBattle_SetAutoSyncCamera( battle_id, TRUE );
+	return battle_id;
+}
+
+static void Game_StartFight( int battle_id )
+{
+	int i;
+	GamePlayer *p_player[4];
+
+	p_player[0] = GameBattle_GetPlayer( battle_id, 1 );
+	p_player[1] = GameBattle_GetPlayer( battle_id, 2 );
+	p_player[2] = GameBattle_GetPlayer( battle_id, 3 );
+	p_player[3] = GameBattle_GetPlayer( battle_id, 4 );
+	
+	/* 设置游戏角色的初始状态 */
+	GamePlayer_SetStart( p_player[0] );
+	GamePlayer_SetStart( p_player[1] );
+	GamePlayer_SetStart( p_player[2] );
+	GamePlayer_SetStart( p_player[3] );
+
+	for(i=0; i<4; ++i) {
+		GameObject_PlayAction( p_player[i]->object );
+		Widget_Show( p_player[i]->object );
+	}
+
+	Widget_Show( GameBattle_GetScene(battle_id) );
+	Game_ShowPlayerStatusArea();
+	GameBattle_InitKeyboardControl();
+	GameBattle_Loop( battle_id );
+}
+
 /** 游戏线程 */
 static void Game_MainThread( void *arg )
 {
-	int role_id;
-	ControlKey ctrlkey;
+	int role_id[4];
+	int main_battle;
 	LCUI_Thread t;
 
 	srand((unsigned int)time(NULL));
+	/* 注册游戏所需的部件类型 */
 	GameObject_Register();
 	GameMenuBtn_Register();
 	GameMenu_Register();
+	StatusBar_Register();
+	LifeBar_Regiser();
 
-	Game_InitAttackRecord();
+	Game_InitAttackLibrary();
 	Game_InitStateActionMap();
 	SkillLibrary_Init();
 
@@ -227,31 +457,15 @@ static void Game_MainThread( void *arg )
 	GameMsgLoopStart();
 	LCUIThread_Create( &t, Game_DemoThread, NULL );
 	while(1) {
-		role_id = Game_GetSelectedRole();	/* 获取选择的角色的ID */
-		if( role_id == -1 ) {
+		role_id[0] = Game_GetSelectedRole();
+		if( role_id[0] == -1 ) {
 			LCUI_MSleep(100);
 			continue;
 		}
-#ifdef aaaa
-		Game_HideMainUI();			/* 隐藏主菜单界面 */
-		/* 设置1号玩家的控制键 */
-		ctrlkey.a_attack = LCUIKEY_J;
-		ctrlkey.b_attack = LCUIKEY_K;
-		ctrlkey.jump = LCUIKEY_SPACE;
-		ctrlkey.defense = LCUIKEY_L;
-		ctrlkey.left = LCUIKEY_A;
-		ctrlkey.right = LCUIKEY_D;
-		ctrlkey.up = LCUIKEY_W;
-		ctrlkey.down = LCUIKEY_S;
 
-		Game_InitBattle();				/* 初始化对战 */
-		Game_EnableGamePlayer( 1 );			/* 启用1号玩家 */
-		Game_SetGamePlayer( 1, role_id, TRUE );		/* 设置1号玩家信息 */
-		Game_SetGamePlayerControlKey( 1, &ctrlkey );	/* 设置1号玩家的控制键 */
-		Game_AddCPUPlayer();				/* 补全剩余玩家信息 */
-		Game_StartBattle();				/* 开始对战 */
-		Game_Loop();					/* 进入游戏主循环 */
-#endif
+		Game_HideMainUI();
+		main_battle = Game_InitFight( role_id );
+		Game_StartFight( main_battle );
 		break;
 	}
 }
@@ -314,7 +528,7 @@ int main( int argc, char **argv )
 	int ret;
 	int mode;
 	LCUI_Thread t;
-//#define DEBUG
+#define DEBUG
 #if defined (LCUI_BUILD_IN_WIN32) && defined (DEBUG)
 	InitConsoleWindow();
 #endif
