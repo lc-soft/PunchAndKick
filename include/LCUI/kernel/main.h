55591a5c110f72269c7699199137b6d5fb66e2d0
Liu Chao <lc-soft@live.cn>
2013-12-01 10:11:10 +0800

    更新LCUI头文件

--- include/LCUI/kernel/main.h
+++ include/LCUI/kernel/main.h
@@ -45,12 +45,13 @@ LCUI_BEGIN_HEADER
 
 /************************ LCUI程序的数据 *******************************/
 typedef struct LCUI_App_ {
-	LCUI_ID id;		/* LCUI程序的ID，如果是以线程方式运行的话，这个就是线程ID */ 
-	void (*func)(void);	/* 在LCUI退出时调用的函数 */
-	LCUI_Queue tasks;	/* 程序的任务队列 */
-	LCUI_Queue events;	/* 事件队列 */
-	LCUI_Queue widget_lib;	/* 部件类型库 */
-	LCUI_Sleeper mainloop_sleeper;
+	LCUI_ID id;			/**< LCUI程序的ID */ 
+	void (*func)(void);		/**< 在LCUI退出时调用的函数 */
+	LCUI_Queue tasks;		/**< 程序的任务队列 */
+	LCUI_Queue events;		/**< 事件队列 */
+	LCUI_Queue widget_lib;		/**< 部件类型库 */
+	LCUI_Sleeper mainloop_sleeper;	/**< 用于决定主循环是否睡眠 */
+	LCUI_Mutex task_mutex;		/**< 任务互斥锁，当运行程序任务时会锁上 */
 }
 LCUI_App;
 /**********************************************************************/
